bbb8b2238e3608a1420f13623c3abd6f1a452f7a
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-02-20 20:00:02 +0800

    feat(Supp2): Be able to draw circles, ellipses, and rectangles. (#3)

--- r2/supp/main.c
+++ r2/supp/main.c
@@ -2,28 +2,52 @@
 #include <stdio.h>
 
 /// \brief Compute the squared norm of a vector `(x, y)`.
-double squaredNorm(double x, double y) {
+double SquaredNorm(double x, double y) {
   return x * x + y * y;
+}
+
+/// \brief Determine whether coordinate `(i, j)` is inside the ellipse, which is
+/// centered at `(centerX, centerY)` and has radius `(radiusX, radiusY)`.
+bool IsInEllipse(int i, int j, double centerX, double centerY, double radiusX, double radiusY) {
+  // Compute center of the pixel `(i, j)`.
+  double x = (double)i + 0.5;
+  double y = (double)j + 0.5;
+
+  double dirX = (x - centerX) / radiusX;
+  double dirY = (y - centerY) / radiusY;
+  return SquaredNorm(dirX, dirY) <= 1.0;
 }
 
 /// \brief Determine whether coordinate `(i, j)` is inside the sphere, which is
 /// centered at `(centerX, centerY)` and has radius `radius`.
-bool isInSphere(int i, int j, double centerX, double centerY, double radius) {
+///
+/// \details You may wonder why not implement `IsInSphere` based on `IsInEllipse`.
+/// That is because multiplications are usually faster than divisions for floating points.
+bool IsInSphere(int i, int j, double centerX, double centerY, double radius) {
   // Compute center of the pixel `(i, j)`.
   double x = (double)i + 0.5;
   double y = (double)j + 0.5;
 
   double dirX = x - centerX;
   double dirY = y - centerY;
-  double dstSquared = squaredNorm(dirX, dirY);
-  return dstSquared <= radius * radius;
+  return SquaredNorm(dirX, dirY) <= radius * radius;
+}
+
+/// \brief Determine whether coordinate `(i, j)` is inside the rectangle, whose
+/// range is from `(minX, minY)` to `(maxX, maxY)`.
+bool IsInRectangle(int i, int j, double minX, double minY, double maxX, double maxY) {
+  // Compute center of the pixel `(i, j)`.
+  double x = (double)i + 0.5;
+  double y = (double)j + 0.5;
+
+  return x >= minX && x <= maxX && y >= minY && y <= maxY;
 }
 
 //
 //
 //
 int main(void) {
-  int imageWidth = 256, imageHeight = 256;
+  int imageWidth = 256, imageHeight = 256 * 0.618;
 
   printf("P3\n%d %d\n255\n", imageWidth, imageHeight);
 
@@ -34,10 +58,13 @@ int main(void) {
       int ig = 0;
       int ib = 0;
 
-      if (isInSphere(i, j, 64.0 * 1.0, 64.0 * 1.0, 64.0) || //
-          isInSphere(i, j, 64.0 * 3.0, 64.0 * 1.0, 64.0) || //
-          isInSphere(i, j, 64.0 * 1.0, 64.0 * 3.0, 64.0) || //
-          isInSphere(i, j, 64.0 * 3.0, 64.0 * 3.0, 64.0)) {
+      double quarterX = (double)imageWidth / 4.0;
+      double quarterY = (double)imageHeight / 4.0;
+
+      if (IsInEllipse(i, j, quarterX * 1.0, quarterY * 1.0, quarterX, quarterY) ||
+          IsInEllipse(i, j, quarterX * 3.0, quarterY * 3.0, quarterX, quarterY) ||
+          IsInRectangle(i, j, quarterX * 0.0, quarterY * 2.0, quarterX * 2.0, quarterY * 4.0) ||
+          IsInRectangle(i, j, quarterX * 2.0, quarterY * 0.0, quarterX * 4.0, quarterY * 2.0)) {
         // r: [0, w - 1] -> [0.0, 1.0].
         // g: [0, h - 1] -> [0.0, 1.0].
         double r = (double)i / (double)(imageWidth - 1);  // `r` = red.
