dc547f4fa1137054126ca77b07ee6296b36570ac
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-02-20 15:19:51 +0800

    .

---
+++ r1/main_1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+  int imageWidth = 256, imageHeight = 256;
+
+  printf("P3\n%d %d\n255\n", imageWidth, imageHeight);
+
+  for (int j = 0; j < imageHeight; ++j) {
+    for (int i = 0; i < imageWidth; ++i) {
+      // [0, W - 1] -> [0.0, 1.0].
+      double r = (double)i / (double)(imageWidth - 1);  // `r` = red.
+      double g = (double)j / (double)(imageHeight - 1); // `g` = green.
+      double b = 0.25;                                  // `b` = blue.
+
+      // [0.0, 1.0] -> [0, 255].
+      int ir = (int)(255.0 * r);
+      int ig = (int)(255.0 * g);
+      int ib = (int)(255.0 * b);
+
+      printf("%d %d %d\n", ir, ig, ib);
+    }
+  }
+
+  return 0;
+}
