f62bbbbf4fc9ea9e48440010f90ad37abbd080b9
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-02-21 20:14:09 +0800

    .

--- r4/supp/go.py
+++ r4/supp/go.py
@@ -4,25 +4,35 @@ import shutil
 import subprocess
 import sys
 
-time = 5
+# Total time of the video.
+time = 8
+# FPS of the video.
 fps = 30
 
+# Compile the C code.
 subprocess.run(["gcc", sys.argv[1], "-o", "main.exe"])
 
+# Clear and create directory `images`.
 if os.path.exists("images"):
   shutil.rmtree("images")
 os.makedirs("images")
 
+# For each frame (image):
 for frame in range(time * fps):
+  # Execute the C code with `frame` and `fps` as arguments.
   with open("image.ppm", "w") as output_file:
     subprocess.run(["./main.exe", str(frame), str(fps)], stdout=output_file)
 
+  # Save the image as `frame_{frame}.png`.
   image = Image.open("image.ppm")
   image.save(f"images/frame_{frame}.png", "png")
 
+# Delete `video.mp4` if exist.
 if os.path.exists("video.mp4"):
   os.remove("video.mp4")
 
+# Generate `video.mp4` with `ffmpeg`.
+# See https://www.ffmpeg.org.
 subprocess.run([
   "ffmpeg",
   "-framerate", str(fps),
