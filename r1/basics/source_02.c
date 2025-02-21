ed2f3eb8e846d2b0c63a43801298e0fd2ae7d78f
DoLphin <dolph1n_dl@163.com>
2025-02-21 18:07:46 +0800

    .

--- r1/basics/source_02.c
+++ r1/basics/source_02.c
@@ -7,7 +7,7 @@ Recitaion Slide 01 - Historical Artifacts of C - Undeclared functions.
 You should check different behaviors between C and C++ compilers yourself!
 
 In the "main" function, an undeclared (thus of course undefined) function
-is called. If you are using IDE or something like clangd, you should have 
+is called. If you are using IDE or something like clangd, you should have
 noticed some warning signs, such as red underline, telling you that something
 is wrong.
 
@@ -20,37 +20,37 @@ Open your terminal("Ctrl + `" if you are using VSCode) and enter current folder.
 
 */
 
-int main(void){
-    int x = function_undeclared();
-    printf("%d\n", x);
-    return 0;
+int main(void) {
+  int x = function_undeclared();
+  printf("%d\n", x);
+  return 0;
 }
 
 /*
 
-Neither of them will be compiled and generate executable, which is fully expected. 
+Neither of them will be compiled and generate executable, which is fully expected.
 Now pay attention to the difference between these two return messages.
 
-When you use "gcc.exe" to compile, you should see a WARNING made by compiler 
+When you use "gcc.exe" to compile, you should see a WARNING made by compiler
 and then a ERROR made by linker ("ld.exe" -the GNU linker).
 While using "g++.exe", everything terminates in the compilation phase, with
 an ERROR telling you 'function_undeclared' was not declared in this scope.
 
 Why is that?
 
-In C, when you call a function that is not declared, the compiler doesn’t 
-know about it during the compilation phase. The C compiler will let the code 
-pass through but then fails during the linking phase because it can’t find 
-the definition of that function (the linker tries to link the function call to 
+In C, when you call a function that is not declared, the compiler doesn’t
+know about it during the compilation phase. The C compiler will let the code
+pass through but then fails during the linking phase because it can’t find
+the definition of that function (the linker tries to link the function call to
 its definition, but it doesn't exist).
 
-In C++, the compiler checks everything more thoroughly during compilation. 
-It wants to make sure that when a function is called, it has already been 
-declared or defined. So, if there’s an undeclared function, the C++ compiler 
+In C++, the compiler checks everything more thoroughly during compilation.
+It wants to make sure that when a function is called, it has already been
+declared or defined. So, if there’s an undeclared function, the C++ compiler
 will report an error at compile-time, before moving on to the linking phase.
 
-** Main Difference ** 
-C++ compilers are more strict at compile time to catch errors early, 
+** Main Difference **
+C++ compilers are more strict at compile time to catch errors early,
 while C compilers are more lenient and let some issues be caught later during linking.
 
 */
--- r1/basics/source_03.c
+++ r1/basics/source_03.c
@@ -7,41 +7,40 @@ Recitaion Slide 01 - An example: the "A + B" problem.
 
 */
 
+int main(void) {
+  int A, B;
+  char C;
 
-int main(void){
-    int A, B;
-    char C;
+  // Correct Implementation 1
+  scanf("%d%d", &A, &B);
+  // Try follwing input:
+  // "1" + "0" + "SPACE" + "2" + "ENTER"
+  // "1" + "0" + "ENTER" + "2" + "ENTER"
+  // "1" + "0" + "-" + "2" + "ENTER"
+  // "1" + "0" + "-" + "a" + "ENTER"
 
-    // Correct Implementation 1
-    scanf("%d%d", &A, &B);
-    // Try follwing input:
-    // "1" + "0" + "SPACE" + "2" + "ENTER"
-    // "1" + "0" + "ENTER" + "2" + "ENTER"
-    // "1" + "0" + "-" + "2" + "ENTER"
-    // "1" + "0" + "-" + "a" + "ENTER"
+  // Correct Implemantation 2
+  // scanf("%d\n%d", &A, &B);
 
-    // Correct Implemantation 2
-    // scanf("%d\n%d", &A, &B);
+  // Correct Implemantation 3
+  // scanf("%d %d", &A, &B);
 
-    // Correct Implemantation 3
-    // scanf("%d %d", &A, &B);
+  // Incorrect Implemantation 1
+  // scanf("%d\n%d\n", &A, &B);
 
-    // Incorrect Implemantation 1
-    // scanf("%d\n%d\n", &A, &B);
+  // Why omitting white space is OK?
+  // Try below:
+  // scanf("%d", &A);
+  // scanf("%d", &B);
 
-    // Why omitting white space is OK?
-    // Try below:
-    // scanf("%d", &A);
-    // scanf("%d", &B);
+  printf("A = %d\nB = %d\nA + B = %d\n", A, B, A + B);
 
-    printf("A = %d\nB = %d\nA + B = %d\n", A, B, A + B);
+  // Read cppreference.com:
+  // Most conversion specifiers first consume all consecutive whitespace.
+  // Exeptions exist, such as "%c".
+  // Try:
+  // scanf("%c", &C);
+  // printf("C = %c\n", C);
 
-    // Read cppreference.com:
-    // Most conversion specifiers first consume all consecutive whitespace.
-    // Exeptions exist, such as "%c".
-    // Try:
-    // scanf("%c", &C);
-    // printf("C = %c\n", C);
-
-    return 0;
+  return 0;
 }
