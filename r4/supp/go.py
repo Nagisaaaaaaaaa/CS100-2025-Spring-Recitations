c523a21439f2c757d15554d50d86f4294d3c15ff
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-02-21 16:43:31 +0800

    .

--- r4/supp/go.py
+++ r4/supp/go.py
@@ -1,11 +1,33 @@
+import os
 from PIL import Image
+import shutil
 import subprocess
 import sys
 
 subprocess.run(["gcc", sys.argv[1], "-o", "main.exe"])
 
-with open("image.ppm", "w") as output_file:
-  subprocess.run(["./main.exe"], stdout=output_file)
+if os.path.exists("images"):
+  shutil.rmtree("images")
+os.makedirs("images")
 
-image = Image.open("image.ppm")
-image.show()
+for frame in range(30):
+  with open("image.ppm", "w") as output_file:
+    subprocess.run(["./main.exe", str(frame)], stdout=output_file)
+
+  image = Image.open("image.ppm")
+  image.save(f"images/frame_{frame}.png", "png")
+
+if os.path.exists("video.mp4"):
+  os.remove("video.mp4")
+
+subprocess.run([
+  "ffmpeg",
+  "-framerate", "15",
+  "-i", "images/frame_%d.png",
+  "-c:v", "libx264",
+  "-profile:v", "high",
+  "-crf", "20",
+  "-pix_fmt", "yuv420p",
+  "video.mp4"
+], check=True)
+shutil.rmtree("images")
