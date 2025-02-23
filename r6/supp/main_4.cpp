3ea137b734c71f712f86ce8bdd29a9c139073a47
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-02-23 18:56:44 +0800

    .

--- r6/supp/main_4.cpp
+++ r6/supp/main_4.cpp
@@ -1,3 +1,44 @@
+/// \file
+/// \brief This file shows that, C++ codes are prone to memory leaks
+/// because of the exceptions.
+
 //
-// Created by 19703 on 2025/2/23.
 //
+//
+#include <cstdlib>
+#include <exception>
+#include <iostream>
+#include <string>
+
+/// \brief A buggy function which will throw `std::invalid_argument` when executed.
+void SomeBuggyFunction() {
+  int num = std::stoi("杂鱼♥～杂鱼♥～，才不会给你一个数字呢♥");
+  std::cout << num << std::endl;
+}
+
+//
+//
+//
+int main() {
+  try {
+    int *const ptr = (int *)malloc(10 * sizeof(int));
+
+    for (int i = 0; i < 10; ++i) {
+      ptr[i] = i;
+      std::cout << ptr[i] << std::endl;
+    }
+
+    // An exception will be thrown here.
+    SomeBuggyFunction();
+
+    //! These lines will not be called because of the exception.
+    printf("This `printf` will not be executed\n");
+    //! Memory leak!
+    free(ptr);
+  } catch (const std::invalid_argument &e) {
+    const char *message = e.what();
+    std::cout << "The exception message is: " << message << std::endl;
+  }
+
+  return 0;
+}
