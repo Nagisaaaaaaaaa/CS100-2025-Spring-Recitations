0ae58d6ff0e393d4e67d96ccd261cf167dcb3b75
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-02-23 16:14:25 +0800

    .

--- r6/supp/main.cpp
+++ r6/supp/main.cpp
@@ -5,7 +5,7 @@
 /// the instance is out of scope.
 struct MemoryGuard {
   // We use `nullptr` instead of `NULL` in C++ codes.
-  int *ptr = nullptr;
+  int *const ptr = nullptr;
 
   // Just like python, C++ allows us to define methods (member functions) of a `struct`.
   // The following method, `~MemoryGuard`, is a special method called "destructor".
@@ -21,7 +21,7 @@ struct MemoryGuard {
 //
 int main() {
   MemoryGuard guard{(int *)malloc(10 * sizeof(int))};
-  int *ptr = guard.ptr;
+  int *const ptr = guard.ptr;
 
   for (int i = 0; i < 10; ++i)
     ptr[i] = i;
