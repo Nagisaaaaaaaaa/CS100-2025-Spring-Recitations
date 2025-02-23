f2e2369a71e01bce7e3e657af38566ea0d859050
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-02-23 18:54:49 +0800

    .

---
+++ r6/supp/main_2.cpp
@@ -0,0 +1,44 @@
+/// \file
+/// \brief This file shows that, C/C++ codes are prone to memory leaks
+/// because of the early returns.
+
+//
+//
+//
+#include <cstdlib>
+#include <iostream>
+
+//
+//
+//
+void DoSomething(bool earlyReturn) {
+  int *const ptr1 = (int *)malloc(1 * sizeof(int));
+  int *const ptr2 = (int *)malloc(2 * sizeof(int));
+  int *const ptr3 = (int *)malloc(3 * sizeof(int));
+  int *const ptr4 = (int *)malloc(4 * sizeof(int));
+  int *const ptr5 = (int *)malloc(5 * sizeof(int));
+
+  if (earlyReturn) {
+    //! WTF, too many memorys to free...
+    //! It's really easy to forget something...
+    free(ptr1);
+    free(ptr2);
+    free(ptr4);
+    free(ptr5);
+
+    return;
+  }
+
+  free(ptr1);
+  free(ptr2);
+  free(ptr3);
+  free(ptr4);
+  free(ptr5);
+}
+
+int main() {
+  DoSomething(false);
+  DoSomething(true);
+
+  return 0;
+}
