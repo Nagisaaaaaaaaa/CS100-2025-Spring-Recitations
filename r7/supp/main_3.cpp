2b5d1e87afa586023cffc907053ee811cdb6abdf
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-02-26 16:02:24 +0800

    .

--- r7/supp/main_3.cpp
+++ r7/supp/main_3.cpp
@@ -2,7 +2,7 @@
 /// \brief This file shows one of the most notorious STL design:
 /// `std::vector<bool>`.
 /// See https://en.cppreference.com/w/cpp/container/vector_bool.
-/// Crazy!
+/// Crazy-terrible design!
 
 //
 //
@@ -20,6 +20,10 @@ int main() {
     std::cout << v << std::endl;
     vec[0] = 466;
     std::cout << v << std::endl;
+
+    for (auto &v : vec)
+      v = 233;
+    std::cout << vec[0] << std::endl;
   }
 
   // For vector of `bool`s.
@@ -30,6 +34,17 @@ int main() {
     std::cout << typeid(v).name() << std::endl;
     std::cout << v << std::endl;
     vec[0] = false;
-    std::cout << v << std::endl; //! ???
+    //! The value has been changed!
+    std::cout << v << std::endl;
+
+    //! Even unable to compile the following `for`-loop!
+    //! Cannot bind non-const lvalue reference of type ... to an rvalue of type ...
+    // for (auto &v : vec)
+    //   v = true;
+
+    //! Have to use `auto v` instead of `auto &v` here!
+    for (auto v : vec)
+      v = true;
+    std::cout << vec[0] << std::endl;
   }
 }
