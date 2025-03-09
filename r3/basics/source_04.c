5195662ee02839fe46cb763e3cf54ec11a287fd1
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-03-09 20:57:01 +0800

    .

--- r3/basics/source_04.c
+++ r3/basics/source_04.c
@@ -1,3 +1,13 @@
+/*
+
+  This piece of source code corresponds to:
+  Recitation Slide 03 - Preprocessor Directives.
+
+  * BONUS:
+  * 1. It's usually OK to use `#pragma once`.
+
+*/
+
 #include "include_04.h"
 #include "include_04.h" // Why including twice is still safe ?
 
