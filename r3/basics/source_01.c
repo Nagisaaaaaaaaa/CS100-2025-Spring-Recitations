36c7a32cf715294716230e628a8087817402ad22
DoLphin <113995234+DoLph1nnn@users.noreply.github.com>
2025-03-10 22:06:41 +0800

    r3 basics and outline added. (#21)
    * .
    
    * .
    
    * .
    
    * .

---
+++ r3/basics/source_01.c
@@ -0,0 +1,80 @@
+/*
+
+This piece of source code corresponds to:
+Recitaion Slide 03 - Control flow.
+
+! BASELINE:
+! 1. You should be familiar with the control flow of the following structure:
+!    Conditional： if-else, switch-case
+!    Loops:        while-loop, for-loop, do-while
+! 2. You should be able to anticipate the behavior for EACH of the given examples.
+! 3. Try follow TA in recitation class and use GNU debugger for Example 2.
+* BONUS:
+* 4. Learn basic console GDB commands such as "break", "continue", "next", "watch", "print"...
+
+*/
+
+#include <stdio.h>
+
+int main(void) {
+
+  // Example 1: Switch case and name-lookup.
+#if 1
+  int i = 10;
+  switch (i) {
+  case 1:
+    int y = 7;
+  case 10:
+    // Line "int y = 7;" will be skipped.
+    // Is "y" a legal variable to be accessed?
+    printf("%d\n", y);
+  default:
+    break;
+  }
+#endif
+
+  // Example 2: Switch fall-through demonstration.
+#if 0
+  for (int i = 0; i < 20; ++i) {
+    switch (i) {
+    case 0:
+      i += 5; // Where will it go?
+    case 1:
+      i += 2;
+    case 5:
+      i += 5;
+    default:
+      i += 4;
+      break;
+    }
+    printf("%d\n", i);
+  }
+#endif
+
+  // Example 3: For-loop and bitwise operator.
+#if 0
+  for (int i = 1024, a = 1; i; i >>= 1, ++a) {
+    printf("Say Hi! --No.%d\n", a);
+  }
+#endif
+
+  // Example 4: When is the updation expression in for-loop executed?
+#if 0
+  for (int n = 9; n != 0; n--) {
+    // What happens when n = 1?
+    printf("n=%d\n", n--);
+  }
+#endif
+
+  // Example 5: Loop corner case.
+#if 0
+  int c = 5;
+  int n = 10000;
+  do {
+    n /= c;
+  } while (c--);
+  printf("%d\n", n);
+#endif
+
+  return 0;
+}
---
+++ r3/basics/source_02.c
@@ -0,0 +1,79 @@
+/*
+
+This piece of source code corresponds to:
+Recitaion Slide 03 - Pointers and arrays.
+
+! BASELINE:
+! 1. You should be able to answer the 9 questions on the recitation slide.
+! 2. You should FULLY undertstand Takeaway #1.
+! 3. You should FULLY remember Takeaway #2 and be able to:
+!    independently answer Lec.06-p33 and r.03-p40, and
+!    independently determine some complicated nested type.
+! 4. Understand how arrays are passed in function parameters.
+! 5. Bear in mind that what you learn today is just theoritical.
+!    It helps you grow profound understanding of pointers and arrays.
+!    But it is BY NO MEANS the encouraged code style.
+
+
+*/
+#include <stdio.h>
+
+void testFunc(int *arr[30][10]) {
+  printf("@testFunc: Param accepted !\n");
+}
+
+int main(void) {
+  // Define an array, holding 100 integers
+  int arr[100] = {42, 114514, 6657, 10086, 2025};
+
+  // Define an integer, and a pointer points to it
+  int a = 42;
+  int *a_ptr = &a;
+
+  // Check the size of int[100] (arr) and int* (a_ptr)
+#if 1
+  printf("The size of int[100] is %d Bytes.\n", (int)sizeof(arr));
+  printf("The size of int *    is %d Bytes.\n", (int)sizeof(a_ptr));
+#endif
+
+  // ! Takeaway #1:
+  // ! An array is NOT a pointer.
+  // ! A pointer is NOT an array.
+  // ! What bridges the two concepts is the fact that:
+  // ! Array can be implicity converted to a pointer (to its first element).
+  // ! Suppose T is a type. T[N] => T* (the type of &T[0]).
+
+#if 0
+  a_ptr = arr;            // What happens here? a_ptr <- {int * (&arr[0]) <= int[100] (arr)}
+  int res = *(a_ptr + 3); // Pointer arithmetic, covered in class.
+  printf("%d\n", res);
+#endif
+
+// Try Casting between pointer types
+#if 0
+  float b = 3.14F;
+  float *b_ptr = &b;
+  a_ptr = b_ptr; // Force a_ptr points to where a float is stored
+  printf("%d\n", *a_ptr);
+#endif
+
+  // What are these horrible-looking types?
+  int *(*ugly_var)[10];
+  int *(**very_ugly_var[10])[42];
+  // How do we interpret them?
+  // ! Takeaway #2:
+  // ! Major principles for interpreting nested types:
+  // ! |----- 1. From inside (near the variable name) to outside.
+  // ! |----- 2. Combine precedence: " [ ] " > " * ", if no "( )" forces.
+  // ! |----- 3. Try both top-down and down-top interpretation.
+  // ! |----- 4. You are required to learn this, but not encouraged to write any code like this.
+
+#if 1
+  // Try some function call with array-type params.
+  int *(*arr_1)[10] = {};
+  testFunc(arr_1);
+
+#endif
+
+  return 0;
+}
