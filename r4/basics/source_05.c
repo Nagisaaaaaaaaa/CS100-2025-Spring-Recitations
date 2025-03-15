3bba5b56e6081d97598e756d3c820322e9aaa22f
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-03-15 19:06:31 +0800

    .

---
+++ r4/basics/source_05.c
@@ -0,0 +1,30 @@
+/*
+
+  This piece of source code corresponds to:
+  Recitaion Slide 04 - Arrays.
+
+  ! Baseline:
+  ! 1. ALWAYS use `Func(int *arr)`, NEVER use `Func(int arr[])`, even though they are the same.
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void Func(int arr[]) {
+  // Do something here.
+}
+
+int main(void) {
+  int arr0[10];
+  int *arr1 = malloc(sizeof(int) * 10);
+
+  int *ptr = arr0; // Then, you can use `ptr` as if you are using `arr0`.
+
+  Func(arr0);
+  Func(arr1);
+
+  free(arr1);
+
+  return 0;
+}
