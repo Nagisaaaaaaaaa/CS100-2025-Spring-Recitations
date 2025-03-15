68becc0c2937c39835c86f869bd8f3209dfe7b48
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-03-15 19:25:43 +0800

    .

--- r4/basics/source_06.c
+++ r4/basics/source_06.c
@@ -4,13 +4,15 @@
   Recitaion Slide 04 - C Strings Library.
 
   ! Baseline:
-  ! 1. `strlen` means string length.
-  ! 2. All C strings are terminated with '\0', called null-terminated.
+  ! 1. All C strings are terminated with '\0', called null-terminated.
+  ! 2. The usages of `strlen` and `strcpy`.
+  ! 3. Never forget the `+ 1` in `malloc(sizeof(char) * (strlen(str) + 1))`.
 
 */
 
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(void) {
@@ -23,6 +25,15 @@ int main(void) {
   printf("\n");
 
   assert(str[strlen(str)] == '\0');
+
+  char buffer0[100];
+  strcpy(buffer0, str);
+  printf("%s\n", buffer0);
+
+  char *buffer1 = malloc(sizeof(char) * (strlen(str) + 1));
+  strcpy(buffer1, str);
+  printf("%s\n", buffer1);
+  free(buffer1);
 
   return 0;
 }
