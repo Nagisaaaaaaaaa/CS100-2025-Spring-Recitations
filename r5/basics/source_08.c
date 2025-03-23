725b3388fbd18258ea2bbff2ef760ecf5baacb77
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-03-23 23:54:39 +0800

    .

---
+++ r5/basics/source_08.c
@@ -0,0 +1,13 @@
+/*
+
+  This piece of source code corresponds to:
+  Recitation Slide 05 - Redirection.
+
+  ! Baseline:
+  ! 1. How to redirect with `>` and `<`.
+  * Bonus:
+  * 2. How to paint images with `printf` and `>`.
+
+*/
+
+//! Please read the codes of `r1/supp`.
