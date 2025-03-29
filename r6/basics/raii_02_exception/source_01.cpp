ba4d8232d1d87c75283f2915637fe55090cdd7d3
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-03-29 19:47:07 +0800

    .

--- r6/basics/raii_02_exception/source_01.cpp
+++ r6/basics/raii_02_exception/source_01.cpp
@@ -1,11 +1,11 @@
 /*
 
   ! Baseline:
-  ! 1. 所谓的“错误处理方式”，除了“error code”之外，还有一种叫做“exception” (异常)。
+  ! 1. “错误处理方式”除了“error code”之外，还有一种叫做“exception” (异常)。
   ! 2. error code v.s. exception:
   !   2.1. error code: 错误发生时，返回非 0 值。
   !   2.2. exception : 错误发生时，一拳给程序打个“半死”。
-  ! 3. C 使用 error code ，而 C++ 和 Python 使用 exception。
+  ! 3. C、Rust 选择了 error code，而 C++、Python 选择了 exception。
 
 */
 
