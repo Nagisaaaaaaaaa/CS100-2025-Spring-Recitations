b71188c0a4dd54d3637c5c4130ea0bfdac21042a
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-03-29 16:27:26 +0800

    .

--- r6/basics/raii_02_exception/source_01.cpp
+++ r6/basics/raii_02_exception/source_01.cpp
@@ -4,20 +4,20 @@
   ! 1. 所谓的“错误处理方式”，除了“error code”之外，还有一种叫做“exception”。
   ! 2. error code v.s. exception:
   !   2.1. error code: 错误发生时，返回非 0 值。
-  !   2.2. exception : 错误发生时，一拳给程序打个半死。
+  !   2.2. exception : 错误发生时，一拳给程序打个“半死”。
   ! 3. C 使用 error code ，而 C++ 和 Python 使用 exception。
 
 */
 
-#include <cstdio>
+#include <iostream>
 #include <vector>
 
 int main() {
-  printf("Start allocating\n");
+  std::cout << "Start allocating" << std::endl;
 
   std::vector<int> v(0xFFFFFFFFFFFFFFFLLU);
 
-  printf("Finished allocating\n");
+  std::cout << "Finished allocating" << std::endl;
 
   return 0;
 }
---
+++ r6/basics/raii_02_exception/source_02.cpp
@@ -0,0 +1,28 @@
+/*
+
+  ! Baseline:
+  ! 1. 所谓的“半死”，意思是，可以选择“原地结束”，或者“起死回生”。
+  !   1.1. “原地结束”：不需要写额外一行代码。
+  !   1.2. “起死回生”：死在 `try` 里面，复活在 `catch` 里面。
+
+*/
+
+#include <iostream>
+#include <vector>
+
+int main() {
+  try {
+    std::cout << "Start allocating" << std::endl;
+
+    std::vector<int> v(0xFFFFFFFFFFFFFFFLLU);
+
+    std::cout << "Finished allocating" << std::endl;
+  } catch (std::exception &e) {
+    // This line prints the exception message.
+    std::cout << "The exception message is: " << e.what() << std::endl;
+  }
+
+  std::cout << "All done" << std::endl;
+
+  return 0;
+}
