069889a05074f43ac71f0530509fb36fcd94746c
DoLphin <dolph1n_dl@163.com>
2025-03-31 16:10:10 +0800

    .

--- r6/basics/source_01.c
+++ r6/basics/source_01.c
@@ -4,113 +4,61 @@
   Recitaion 06 - Struct.
 
   ! Baseline:
-  ! 1. Learn how to use struct to package a bunch of input / output variables.
-  ! 2. Get familiar with the provided examples.
+  ! 1. Get familiar with examples below.
 
 */
 
+#include <float.h>
 #include <stdbool.h>
 #include <stdio.h>
 
-// Toy example: Interval
-typedef struct {
-  float left;
-  float right;
-} Interval;
-
 // Navie implementation, correct but messy.
-bool contain(float x, float min, float max) {
+bool contain_(double x, double min, double max) {
   return (min <= x) && (x <= max);
 }
 
+typedef struct {
+  double left;
+  double right;
+} Interval;
+
 // Better version. Different floats are separated.
-bool contain_pro(float x, Interval intv) {
+bool contain(double x, Interval intv) {
   return (intv.left <= x) && (x <= intv.right);
 }
+
+//clang-format off
+double clamp(double x, Interval intv) {
+  if (x > intv.right)
+    return intv.right;
+  if (x < intv.left)
+    return intv.left;
+  return x;
+}
+
+double span(Interval intv) {
+  double res = intv.right - intv.left;
+  return (res > 0 ? res : 0);
+}
+
+void print_intv(Interval intv) {
+  printf("(%e, %e); span = %e\n", intv.left, intv.right, span(intv));
+}
+
+// maximum span of double representation
+const Interval universe = {.left = -DBL_MAX, .right = +DBL_MAX};
+
+// maximum span of double representation
+const Interval empty = {.left = +DBL_MAX, .right = -DBL_MAX};
 
 // Suppose you want a function to return a triangle in 3D.
 // How would you like to implement it?
 
-// Attempt 1:
-float get_tri_a_x() {
-
-  // ... some complicated computation
-  float a_x = 0.0F; // compute coordinate
-  return a_x;
-}
-
-// However, you need the following as well:
-// float get_tri_a_y() {}
-// float get_tri_a_z() {}
-// float get_tri_b_x() {}
-// float get_tri_b_y() {}
-// float get_tri_b_z() {}
-// float get_tri_c_x() {}
-// float get_tri_c_y() {}
-// float get_tri_c_z() {}
-
-// Attempt 2:
-// clang-format off
-void get_tri_vertices(
-    float *a_x, float *a_y, float *a_z, 
-    float *b_x, float *b_y, float *b_z, 
-    float *c_x, float *c_y, float *c_z) {
-
-    // ... some complicated computation
-    *a_x = 0.0F;
-    *a_y = 1.0F;
-    *a_z = -1.0F;
-    // .. Similar things for the other vertices
-}
-
-// clang-format on
-
-// Attempt 3: struct wrapping
-typedef struct Vertex3D {
-  float px;
-  float py;
-  float pz;
-} v3;
-
-typedef struct Triangle3D {
-  v3 A;
-  v3 B;
-  v3 C;
-} tri3;
-
-// clang-format off
-tri3 get_tri() {
-  // ... some complicated computation
-
-  v3 A = (v3){0, 0, 0};
-  v3 B = (v3){0, 1, 0};
-  v3 C = (v3){1, 0, 0};
-
-  return (tri3){A, B, C};
-}
-
-// clang-format on
-
-// Suppose we want to describe an air-conditioner with C code,
-// whoese status (temperature, on/off, wind-direction) can change.
-// You can avoid the use of scattered variables by defining a struct.
-
-enum AC_wind_dir { Upward, Downward, Mid, Loop };
-
-typedef struct AirConditioner {
-  float temperature;
-  bool turned_on;
-  enum AC_wind_dir wind_dir;
-
-} AC;
-
-void remote_controller(AC *ptrAC /* ... maybe more user input */) {
-  ptrAC->temperature = 25.0F; // adjust temperature..
-  ptrAC->turned_on = true;    // turn on
-  ptrAC->wind_dir = Loop;     // loop wind
-}
-
 int main(void) {
+  Interval A = {10.0, 42.0};
+  print_intv(A);
+  print_intv(universe);
+  print_intv(empty);
 
   return 0;
 }
---
+++ r6/basics/source_03.c
@@ -0,0 +1,81 @@
+/*
+
+  This piece of source code corresponds to:
+  Recitaion 06 - Struct.
+
+  ! Baseline:
+  ! 1. Get familiar with the examples below.
+
+*/
+
+#include <float.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+bool eq(double x, double y) {
+  double diff = x > y ? (x - y) : (y - x);
+  return diff < 1e-8;
+}
+
+typedef struct {
+  double k; // slope
+  double b; // intercept
+} Line2d;
+
+void print_line(Line2d l) {
+  printf("Line: y = %.3f * x + %.3f\n", l.k, l.b);
+}
+
+enum pattern { parallel, intersect, overlap };
+
+// What is the advantage of this struct?
+typedef struct {
+  enum pattern has_hit;
+  double hit_x;
+  double hit_y;
+} HitRecord;
+
+void print_hitrec(HitRecord rec) {
+  switch (rec.has_hit) {
+  case parallel:
+    printf("Lines are parallel!\n");
+    break;
+  case overlap:
+    printf("Lines overlap!\n");
+    break;
+  case intersect:
+    printf("Lines intersect at (%.3f, %.3f)!\n", rec.hit_x, rec.hit_y);
+    break;
+  }
+}
+
+HitRecord check_intersect(Line2d l1, Line2d l2) {
+  HitRecord rec;
+  rec.hit_x = 0.0;
+  rec.hit_y = 0.0;
+  if (eq(l1.k, l2.k)) {
+    // overlap
+    if (eq(l1.b, l2.b)) {
+      rec.has_hit = overlap;
+      return rec;
+    }
+    // parallel
+    rec.has_hit = parallel;
+    return rec;
+  }
+  // intersect
+  rec.has_hit = intersect;
+  rec.hit_x = (l1.b - l2.b) / (l2.k - l1.k);
+  rec.hit_y = l1.k * rec.hit_x + l1.b;
+  return rec;
+}
+
+int main(void) {
+  Line2d l1 = {.k = 1.0, .b = 0.0};
+  Line2d l2 = {.k = 0.0, .b = 1.0};
+  print_line(l1);
+  print_line(l2);
+  print_hitrec(check_intersect(l1, l2));
+
+  return 0;
+}
