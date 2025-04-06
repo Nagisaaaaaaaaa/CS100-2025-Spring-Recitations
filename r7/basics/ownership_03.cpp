cc62133a40ffc94cf084f5f58cd3c29d94776278
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-04-07 02:09:57 +0800

    .

---
+++ r7/basics/ownership_03.cpp
@@ -0,0 +1,25 @@
+/*
+
+  ! Baseline:
+  ! 1. Who owns the array in this example?
+  ! 2. Why?
+
+*/
+
+#include <stdlib.h>
+
+struct Vector {
+  int *data;
+
+  ~Vector() { free(data); }
+};
+
+int main() {
+  //! `vec` owns the array.
+  Vector vec{(int *)malloc(sizeof(int) * 5)};
+
+  //! `data` does not own the array.
+  int *data = vec.data;
+
+  return 0;
+}
