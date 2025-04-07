8ae093d1885681370ee8b721807f3d764969baa4
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-04-07 15:22:31 +0800

    .

--- r7/supp/main.cpp
+++ r7/supp/main.cpp
@@ -1,5 +1,41 @@
 #include "ARIA/Registry.h"
 
+#include <vector>
+
+struct Vec {
+  int x, y;
+};
+
+struct Tank : public ARIA::Registry<Tank> {
+  Vec pos;
+
+  using Base = Registry<Tank>;
+  using Base::crange;
+  using Base::range;
+  using Base::size;
+};
+
 int main() {
+  // 0.
+  std::cout << Tank::size() << std::endl;
+
+  {
+    // 0 + 1 = 1.
+    Tank tank;
+    std::cout << Tank::size() << std::endl;
+
+    {
+      // 1 + 5 = 6.
+      std::vector<Tank> tanks(5);
+      std::cout << Tank::size() << std::endl;
+    }
+
+    // 6 - 5 = 1.
+    std::cout << Tank::size() << std::endl;
+  }
+
+  // 1 - 1 = 0.
+  std::cout << Tank::size() << std::endl;
+
   return 0;
 }
