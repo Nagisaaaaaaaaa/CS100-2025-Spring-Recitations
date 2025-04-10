676a37aa7d52d95e21f7bff6ba6bf21219e37875
Yiheng Wu <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-04-10 20:55:35 +0800

    feat(R8): Use `ARIA`'s `Tup` as supp. (#41)

---
+++ r8/supp/ARIA/Tup.h
@@ -0,0 +1,263 @@
+#pragma once
+
+/// \file
+/// \brief A tuple abstraction based on NVIDIA CuTe.
+/// See https://github.com/NVIDIA/cutlass/blob/main/media/docs/cute/00_quickstart.md.
+///
+/// CuTe is a collection of C++ CUDA template abstractions for
+/// defining and operating on hierarchically multidimensional layouts of threads and data.
+/// CuTe provides `Layout` and `Tensor` objects that
+/// compactly packages the type, shape, memory space, and layout of data, while
+/// performing the complicated indexing for the user.
+/// This lets programmers focus on the logical descriptions of their algorithms while
+/// CuTe does the mechanical bookkeeping for them.
+/// With these tools, we can quickly design, implement, and modify all dense linear algebra operations.
+///
+/// The core abstraction of CuTe are the hierarchically multidimensional layouts which
+/// can be composed with data arrays to represent tensors.
+/// The representation of layouts is powerful enough to represent nearly everything
+/// we need to implement efficient dense linear algebra.
+/// Layouts can also be combined and manipulated via functional composition, on which
+/// we build a large set of common operations such as tiling and partitioning.
+///
+/// This file introduces `Tup` (tuple) and `Tec` (tuple + vec), where
+/// `Tec` is a vec type which can be fully or even partially determined at compile time.
+/// `Tup` and `Tec` are extensively used in ARIA metaprogramming.
+///
+/// \note This file is not fully documented, since we assume that the users have basic knowledge about CuTe.
+/// If you are not familiar with CuTe, please read the tutorials of NVIDIA CuTe before continue:
+/// 0. https://github.com/NVIDIA/cutlass/blob/main/media/docs/cute/00_quickstart.md.
+/// 1. https://github.com/NVIDIA/cutlass/blob/main/media/docs/cute/01_layout.md.
+/// 2. ...
+
+//
+//
+//
+//
+//
+// #include "ARIA/detail/TupImpl.h"
+#include "./detail/TupImpl.h"
+
+namespace ARIA {
+
+/// \brief `Tup` (tuple) is a fixed-size collection of heterogeneous values.
+///
+/// \example ```cpp
+/// Tup tup{1, C<2U>{}, Tup{3.0F, std::string{"4"}}};
+/// ```
+///
+/// \warning `Tup` has the same type as `cute::tuple`.
+using tup::detail::Tup;
+
+/// \brief `Tec` (tuple + vec) is a fixed-size collection of heterogeneous "arithmetic" values.
+///
+/// \example ```cpp
+/// Tec tec{1, C<2U>{}, 3.0F, C<4.0>{}};
+/// Tec3 tec3{1, C<2U>{}, 3.0F};
+/// Teci teci{1, C<2>{}, 3, C<4>{}};
+/// Tec3i tec3i{1, C<2>{}, 3};
+///
+/// Tec3i a = {1, 2_I, 3};
+/// Tec3i b = {0, 2_I, 4};
+/// Tec3i c = a + b; // `c` contains `1`, `4_I`, `7`.
+/// ```
+///
+/// \warning `Tec` has the same type as `cute::tuple` but with restrictions, where
+/// the elements are required to be "arithmetic" types.
+/// Here, "arithmetic" means `std::is_arithmetic` or `ConstantArithmetic`.
+using tup::detail::Tec;
+
+//
+//
+//
+/// \brief Make a `Tec` with the given functor.
+///
+/// \example ```cpp
+/// // Type of `v0` and `v1` is `Tec<C<0>, C<3>, C<6>>`.
+/// constexpr Tec v0 = make_tec<3>([](auto i) { return i * 3_I; });
+/// constexpr Tec v1 = make_tec<3>([]<auto i> { return i * 3_I; });
+///
+/// // Type of `v2` and `v3` is `Tec<C<0>, int, int>`.
+/// constexpr Tec v2 = make_tec<3>([](auto i) { return i * 3; });
+/// constexpr Tec v3 = make_tec<3>([]<auto i> { return i * 3; });
+/// ```
+using tup::detail::make_tec;
+
+//
+//
+//
+using tup::detail::Tec1;
+using tup::detail::Tec2;
+using tup::detail::Tec3;
+using tup::detail::Tec4;
+
+using tup::detail::Tecd;
+using tup::detail::Tecf;
+using tup::detail::Teci;
+using tup::detail::Tecr;
+using tup::detail::Tecu;
+
+using tup::detail::Tec1d;
+using tup::detail::Tec1f;
+using tup::detail::Tec1i;
+using tup::detail::Tec1r;
+using tup::detail::Tec1u;
+
+using tup::detail::Tec2d;
+using tup::detail::Tec2f;
+using tup::detail::Tec2i;
+using tup::detail::Tec2r;
+using tup::detail::Tec2u;
+
+using tup::detail::Tec3d;
+using tup::detail::Tec3f;
+using tup::detail::Tec3i;
+using tup::detail::Tec3r;
+using tup::detail::Tec3u;
+
+using tup::detail::Tec4d;
+using tup::detail::Tec4f;
+using tup::detail::Tec4i;
+using tup::detail::Tec4r;
+using tup::detail::Tec4u;
+
+//
+//
+//
+//
+//
+using tup::detail::tup_size_v;
+
+using tup::detail::tup_elem_t;
+
+//
+//
+//
+using tup::detail::rank;
+
+using tup::detail::rank_t;
+
+using tup::detail::rank_v;
+
+using tup::detail::get;
+
+//
+//
+//
+using tup::detail::is_static;
+
+using tup::detail::is_static_v;
+
+//
+//
+//
+//
+//
+/// \brief Cast `Tup` to `TypeArray`.
+///
+/// \example ```cpp
+/// Tup tup{1, C<2U>{}, Tup{3.0F, std::string{"4"}}};
+/// using Ts = to_type_array_t<decltype(tup)>; // TypeArray<int, C<2U>, Tup<float, std::string>>
+/// ```
+using tup::detail::to_type_array_t;
+
+/// \brief Cast `TypeArray` to `Tup`.
+///
+/// \example ```cpp
+/// using Ts = MakeTypeArray<int, C<2U>, Tup<float, std::string>>;
+/// using TTup = to_tup_t<Ts>; // Tup<int, C<2U>, Tup<float, std::string>>
+/// ```
+using tup::detail::to_tup_t;
+
+//
+//
+//
+/// \brief Cast `Tec` to `std::array`.
+///
+/// \example ```cpp
+/// std::array<int, 2> array0 = ToArray(Tec{5, 6});
+/// std::array<int, 2> array1 = ToArray(Tec{5_I, 6});
+/// std::array<int, 2> array2 = ToArray(Tec{5_I, 6_I});
+/// ```
+using tup::detail::ToArray;
+
+//
+//
+//
+//
+//
+/// \brief Define a `Tec` with the same compile-time values.
+///
+/// \example ```cpp
+/// // Type of `v233` is `Tec<C<233>, C<233>, C<233>>`.
+/// constexpr Tec v233 = TecConstant<3, 233>{};
+/// ```
+using tup::detail::TecConstant;
+
+/// \brief Define a `Tec` with compile-time zeros.
+///
+/// \example ```cpp
+/// // Type of `zero` is `Tec<C<0>, C<0>, C<0>>`.
+/// constexpr Tec zero = TecZero<int, 3>{};
+/// ```
+using tup::detail::TecZero;
+
+/// \brief Define a `Tec` with compile-time ones.
+///
+/// \example ```cpp
+/// // Type of `one` is `Tec<C<1>, C<1>, C<1>>`.
+/// constexpr Tec one = TecOne<int, 3>{};
+/// ```
+using tup::detail::TecOne;
+
+/// \brief Define a `Tec` with compile-time `0`, `1`, `2`, ...
+///
+/// \example ```cpp
+/// // Type of `idx` is `Tec<C<0>, C<1>, C<2>>`.
+/// constexpr Tec idx = TecIndexSequence<int, 3>{};
+/// ```
+using tup::detail::TecIndexSequence;
+
+/// \brief Define a `Tec` with one compile-time `1` among zeros.
+///
+/// \example ```cpp
+/// // Type of `unit` is `Tec<C<0>, C<0>, C<1>>`.
+/// constexpr Tec unit = TecUnit<int, 3, 2>{};
+/// ```
+using tup::detail::TecUnit;
+
+//
+//
+//
+//
+//
+/// \brief Compute the dot product of two `Tec`s.
+///
+/// \example ```cpp
+/// Tec v0{1_I, 2_I};
+/// Tec v1{5_I, 6_I};
+/// static_assert(Dot(v0, v1) == 17_I);
+/// static_assert(std::is_same_v<decltype(Dot(v0, v1)), C<17>>);
+/// ```
+using tup::detail::Dot;
+
+/// \brief Compute the squared norm of a `Tec`.
+///
+/// \example ```cpp
+/// Tec v{5_I, 6_I};
+/// static_assert(NormSquared(v) == 61_I);
+/// static_assert(std::is_same_v<decltype(NormSquared(v)), C<61>>);
+/// ```
+using tup::detail::NormSquared;
+
+/// \brief Compute the cross product of two `Tec`s.
+///
+/// \example ```cpp
+/// Tec v0{1_I, 2_I, 3_I};
+/// Tec v1{5_I, 6_I, 7_I};
+/// static_assert(Cross(v0, v1) == Tec{-4_I, 8_I, -4_I});
+/// static_assert(std::is_same_v<decltype(Cross(v0, v1)), Tec<C<-4>, C<8>, C<-4>>>);
+/// ```
+using tup::detail::Cross;
+
+} // namespace ARIA
---
+++ r8/supp/ARIA/detail/TupImpl.h
@@ -0,0 +1,578 @@
+#pragma once
+
+// #include "ARIA/TypeArray.h"
+#include "../TypeArray.h"
+
+// #include <cute/int_tuple.hpp>
+#include "../../cute/int_tuple.hpp"
+
+#include <array>
+
+// Define CTAD for CuTe types.
+namespace cute {
+
+template <typename... Ts>
+tuple(Ts...) -> tuple<Ts...>;
+
+}
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+namespace ARIA {
+
+namespace tup::detail {
+
+// Get the underlying arithmetic domain.
+// Examples:
+//   int         -> int
+//   const int   -> int
+//   const int&  -> int
+//   C<1>        -> int
+//   const C<1>  -> int
+//   const C<1>& -> int
+//   std::string -> void
+template <typename T>
+struct arithmetic_domain {
+  using type = void;
+};
+
+template <typename T>
+  requires(!ConstantArithmetic<std::decay_t<T>> && std::is_arithmetic_v<std::decay_t<T>>)
+struct arithmetic_domain<T> {
+  using type = std::decay_t<T>;
+};
+
+template <typename T>
+  requires(ConstantArithmetic<std::decay_t<T>>)
+struct arithmetic_domain<T> {
+  using type = std::decay_t<decltype(std::decay_t<T>::value)>;
+};
+
+template <typename T>
+using arithmetic_domain_t = typename arithmetic_domain<T>::type;
+
+template <typename T>
+constexpr bool has_arithmetic_domain_v = !std::is_void_v<arithmetic_domain_t<T>>;
+
+//
+//
+//
+template <typename... Ts>
+using Tup = cute::tuple<Ts...>;
+
+template <typename... Ts>
+  requires(has_arithmetic_domain_v<Ts> && ...)
+using Tec = cute::tuple<Ts...>;
+
+//
+//
+//
+template <auto n, typename F, typename... Ts>
+[[nodiscard]] ARIA_HOST_DEVICE constexpr auto make_tec_impl(F &&f, Ts &&...ts) {
+  using TIdx = decltype(n);
+  constexpr TIdx i0(0);
+  constexpr TIdx i1(1);
+
+  if constexpr (n == i0) {
+    return Tec{std::forward<Ts>(ts)...};
+  } else {
+    if constexpr (std::is_invocable_v<F, C<n - i1>>)
+      return make_tec_impl<n - i1>(std::forward<F>(f), f(C<n - i1>{}), std::forward<Ts>(ts)...);
+    else
+      return make_tec_impl<n - i1>(std::forward<F>(f), f.template operator()<n - i1>(), std::forward<Ts>(ts)...);
+  }
+}
+
+template <auto n, typename F>
+[[nodiscard]] ARIA_HOST_DEVICE constexpr auto make_tec(F &&f) {
+  return make_tec_impl<n>(std::forward<F>(f));
+}
+
+//
+//
+//
+//
+//
+// TODO: Great efforts are made to bypass the MSVC bug.
+template <typename... Ts>
+  requires((sizeof...(Ts) == 1 && has_arithmetic_domain_v<Ts>) && ...)
+using Tec1 = cute::tuple<Ts...>;
+
+template <typename... Ts>
+  requires((sizeof...(Ts) == 2 && has_arithmetic_domain_v<Ts>) && ...)
+using Tec2 = cute::tuple<Ts...>;
+
+template <typename... Ts>
+  requires((sizeof...(Ts) == 3 && has_arithmetic_domain_v<Ts>) && ...)
+using Tec3 = cute::tuple<Ts...>;
+
+template <typename... Ts>
+  requires((sizeof...(Ts) == 4 && has_arithmetic_domain_v<Ts>) && ...)
+using Tec4 = cute::tuple<Ts...>;
+
+//
+//
+//
+template <typename... Ts>
+  requires(std::is_same_v<arithmetic_domain_t<Ts>, int> && ...)
+using Teci = cute::tuple<Ts...>;
+
+template <typename... Ts>
+  requires(std::is_same_v<arithmetic_domain_t<Ts>, uint> && ...)
+using Tecu = cute::tuple<Ts...>;
+
+template <typename... Ts>
+  requires(std::is_same_v<arithmetic_domain_t<Ts>, float> && ...)
+using Tecf = cute::tuple<Ts...>;
+
+template <typename... Ts>
+  requires(std::is_same_v<arithmetic_domain_t<Ts>, double> && ...)
+using Tecd = cute::tuple<Ts...>;
+
+template <typename... Ts>
+  requires(std::is_same_v<arithmetic_domain_t<Ts>, Real> && ...)
+using Tecr = cute::tuple<Ts...>;
+
+//
+//
+//
+template <typename... Ts>
+  requires((sizeof...(Ts) == 1 && std::is_same_v<arithmetic_domain_t<Ts>, int>) && ...)
+using Tec1i = cute::tuple<Ts...>;
+
+template <typename... Ts>
+  requires((sizeof...(Ts) == 1 && std::is_same_v<arithmetic_domain_t<Ts>, uint>) && ...)
+using Tec1u = cute::tuple<Ts...>;
+
+template <typename... Ts>
+  requires((sizeof...(Ts) == 1 && std::is_same_v<arithmetic_domain_t<Ts>, float>) && ...)
+using Tec1f = cute::tuple<Ts...>;
+
+template <typename... Ts>
+  requires((sizeof...(Ts) == 1 && std::is_same_v<arithmetic_domain_t<Ts>, double>) && ...)
+using Tec1d = cute::tuple<Ts...>;
+
+template <typename... Ts>
+  requires((sizeof...(Ts) == 1 && std::is_same_v<arithmetic_domain_t<Ts>, Real>) && ...)
+using Tec1r = cute::tuple<Ts...>;
+
+//
+//
+//
+template <typename... Ts>
+  requires((sizeof...(Ts) == 2 && std::is_same_v<arithmetic_domain_t<Ts>, int>) && ...)
+using Tec2i = cute::tuple<Ts...>;
+
+template <typename... Ts>
+  requires((sizeof...(Ts) == 2 && std::is_same_v<arithmetic_domain_t<Ts>, uint>) && ...)
+using Tec2u = cute::tuple<Ts...>;
+
+template <typename... Ts>
+  requires((sizeof...(Ts) == 2 && std::is_same_v<arithmetic_domain_t<Ts>, float>) && ...)
+using Tec2f = cute::tuple<Ts...>;
+
+template <typename... Ts>
+  requires((sizeof...(Ts) == 2 && std::is_same_v<arithmetic_domain_t<Ts>, double>) && ...)
+using Tec2d = cute::tuple<Ts...>;
+
+template <typename... Ts>
+  requires((sizeof...(Ts) == 2 && std::is_same_v<arithmetic_domain_t<Ts>, Real>) && ...)
+using Tec2r = cute::tuple<Ts...>;
+
+//
+//
+//
+template <typename... Ts>
+  requires((sizeof...(Ts) == 3 && std::is_same_v<arithmetic_domain_t<Ts>, int>) && ...)
+using Tec3i = cute::tuple<Ts...>;
+
+template <typename... Ts>
+  requires((sizeof...(Ts) == 3 && std::is_same_v<arithmetic_domain_t<Ts>, uint>) && ...)
+using Tec3u = cute::tuple<Ts...>;
+
+template <typename... Ts>
+  requires((sizeof...(Ts) == 3 && std::is_same_v<arithmetic_domain_t<Ts>, float>) && ...)
+using Tec3f = cute::tuple<Ts...>;
+
+template <typename... Ts>
+  requires((sizeof...(Ts) == 3 && std::is_same_v<arithmetic_domain_t<Ts>, double>) && ...)
+using Tec3d = cute::tuple<Ts...>;
+
+template <typename... Ts>
+  requires((sizeof...(Ts) == 3 && std::is_same_v<arithmetic_domain_t<Ts>, Real>) && ...)
+using Tec3r = cute::tuple<Ts...>;
+
+//
+//
+//
+template <typename... Ts>
+  requires((sizeof...(Ts) == 4 && std::is_same_v<arithmetic_domain_t<Ts>, int>) && ...)
+using Tec4i = cute::tuple<Ts...>;
+
+template <typename... Ts>
+  requires((sizeof...(Ts) == 4 && std::is_same_v<arithmetic_domain_t<Ts>, uint>) && ...)
+using Tec4u = cute::tuple<Ts...>;
+
+template <typename... Ts>
+  requires((sizeof...(Ts) == 4 && std::is_same_v<arithmetic_domain_t<Ts>, float>) && ...)
+using Tec4f = cute::tuple<Ts...>;
+
+template <typename... Ts>
+  requires((sizeof...(Ts) == 4 && std::is_same_v<arithmetic_domain_t<Ts>, double>) && ...)
+using Tec4d = cute::tuple<Ts...>;
+
+template <typename... Ts>
+  requires((sizeof...(Ts) == 4 && std::is_same_v<arithmetic_domain_t<Ts>, Real>) && ...)
+using Tec4r = cute::tuple<Ts...>;
+
+//
+//
+//
+// Whether the given type is `Tec<...>`.
+template <typename T>
+struct is_tec : std::false_type {};
+
+template <typename... Ts>
+struct is_tec<Tec<Ts...>> : std::true_type {};
+
+template <typename T>
+constexpr bool is_tec_v = is_tec<T>::value;
+
+//
+//
+//
+// Whether the given `Tec` has rank `r`.
+template <typename TTec, auto r>
+struct is_tec_r : std::false_type {};
+
+template <typename... Ts, auto r>
+  requires(sizeof...(Ts) == r)
+struct is_tec_r<Tec<Ts...>, r> : std::true_type {};
+
+template <typename TTec, auto r>
+constexpr bool is_tec_r_v = is_tec_r<TTec, r>::value;
+
+//
+//
+//
+// Whether the given `Tec` has "arithmetic" type `T`.
+template <typename TTec, typename T>
+struct is_tec_t : std::false_type {};
+
+template <typename... Ts, typename T>
+  requires(std::is_same_v<arithmetic_domain_t<Ts>, T> && ...)
+struct is_tec_t<Tec<Ts...>, T> : std::true_type {};
+
+template <typename TTec, typename T>
+constexpr bool is_tec_t_v = is_tec_t<TTec, T>::value;
+
+//
+//
+//
+// Whether the given `Tec` has "arithmetic" type `T` and rank `r`.
+template <typename TTec, typename T, auto r>
+struct is_tec_tr : std::false_type {};
+
+template <typename... Ts, typename T, auto r>
+  requires(sizeof...(Ts) == r && (std::is_same_v<arithmetic_domain_t<Ts>, T> && ...))
+struct is_tec_tr<Tec<Ts...>, T, r> : std::true_type {};
+
+template <typename TTec, typename T, auto r>
+constexpr bool is_tec_tr_v = is_tec_tr<TTec, T, r>::value;
+
+//
+//
+//
+//
+//
+template <typename T>
+constexpr auto tup_size_v = cute::tuple_size_v<T>;
+
+template <auto I, typename T>
+using tup_elem_t = cute::tuple_element_t<I, T>;
+
+//
+//
+//
+using cute::rank;
+using cute::rank_t;
+using cute::rank_v;
+
+using cute::get;
+
+//
+//
+//
+using cute::is_static;
+using cute::is_static_v;
+
+//
+//
+//
+//
+//
+// Cast `Tup` to `TypeArray`.
+template <typename... Ts>
+consteval auto ToTypeArray(const Tup<Ts...> &) {
+  return MakeTypeArray<Ts...>{};
+}
+
+template <typename TTup>
+using to_type_array_t = decltype(detail::ToTypeArray(std::declval<TTup>()));
+
+// Cast `TypeArray` to `Tup`.
+template <typename... Ts>
+consteval auto ToTup(const TypeArray<Ts...> &) {
+  return Tup<Ts...>{};
+}
+
+template <typename TArray>
+using to_tup_t = decltype(detail::ToTup(std::declval<TArray>()));
+
+//
+//
+//
+// Cast `Tec` to `std::array`.
+template <typename T, typename... Ts>
+[[nodiscard]] ARIA_HOST_DEVICE static constexpr auto ToArray(const Tec<T, Ts...> &tec) {
+  using value_type = arithmetic_domain_t<T>;
+  static_assert(is_tec_t_v<Tec<T, Ts...>, value_type>, "Element types of `Tec` should be \"as similar as possible\"");
+
+  constexpr uint rank = rank_v<Tec<T, Ts...>>;
+
+  std::array<value_type, rank> res;
+  ForEach<rank>([&]<auto i>() { res[i] = get<i>(tec); });
+  return res;
+}
+
+//
+//
+//
+//
+//
+// Commonly-used constants.
+template <uint n, auto v, auto... vs>
+consteval auto ConstantImpl() {
+  if constexpr (n == 0)
+    return Tec<C<vs>...>{};
+  else
+    return ConstantImpl<n - 1, v, v, vs...>();
+}
+
+template <typename T, uint n, auto... vs>
+consteval auto IndexSequenceImpl() {
+  if constexpr (n == 0)
+    return Tec<C<T(vs)>...>{};
+  else
+    return IndexSequenceImpl<T, n - 1, n - 1, vs...>();
+}
+
+template <typename T, uint n, uint i, auto... vs>
+consteval auto UnitImpl() {
+  if constexpr (n == 0)
+    return Tec<C<T(vs)>...>{};
+  else
+    return UnitImpl<T, n - 1, i, (n == i + 1 ? 1 : 0), vs...>();
+}
+
+template <uint n, auto v>
+using TecConstant = decltype(ConstantImpl<n, v>());
+
+template <typename T, uint n>
+using TecZero = decltype(ConstantImpl<n, T(0)>());
+
+template <typename T, uint n>
+using TecOne = decltype(ConstantImpl<n, T(1)>());
+
+template <typename T, uint n>
+using TecIndexSequence = decltype(IndexSequenceImpl<T, n>());
+
+template <typename T, uint n, uint i>
+  requires(i < n)
+using TecUnit = decltype(UnitImpl<T, n, i>());
+
+//
+//
+//
+//
+//
+// Math-related features.
+template <typename... Ts0, typename... Ts1>
+[[nodiscard]] ARIA_HOST_DEVICE constexpr auto Dot(const Tec<Ts0...> &a, const Tec<Ts1...> &b) {
+  static_assert(sizeof...(Ts0) > 0, "Empty `Tec`s are not allowed");
+
+  //! `Ts0` is automatically required to have the same number of elements as `Ts1` at next line.
+  using TRes = decltype(((std::declval<Ts0>() * std::declval<Ts1>()) + ...));
+  if constexpr (is_static_v<TRes>) {
+    return TRes{};
+  } else {
+    TRes res{};
+    ForEach<sizeof...(Ts0)>([&]<auto i>() { res += get<i>(a) * get<i>(b); });
+    return res;
+  }
+}
+
+template <typename... Ts>
+[[nodiscard]] ARIA_HOST_DEVICE constexpr auto NormSquared(const Tec<Ts...> &a) {
+  return Dot(a, a);
+}
+
+template <typename... Ts0, typename... Ts1>
+[[nodiscard]] ARIA_HOST_DEVICE constexpr auto Cross(const Tec<Ts0...> &a, const Tec<Ts1...> &b) {
+  static_assert(sizeof...(Ts0) == 3 && sizeof...(Ts1) == 3,
+                "Cross product is only implemented for `Tec`s with ranks equal to 3");
+
+  return Tec{get<1>(a) * get<2>(b) - get<2>(a) * get<1>(b), //
+             get<2>(a) * get<0>(b) - get<0>(a) * get<2>(b), //
+             get<0>(a) * get<1>(b) - get<1>(a) * get<0>(b)};
+}
+
+} // namespace tup::detail
+
+} // namespace ARIA
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// Arithmetic operators for coords.
+//! WARNING: These operators should only be defined in `namespace cute` in order for
+//! ADL to find the correct operators.
+
+namespace cute {
+
+namespace aria::tup::detail {
+
+// Fill a `Tec` with a same value.
+template <typename... Ts>
+[[nodiscard]] ARIA_HOST_DEVICE constexpr ARIA::tup::detail::Tec<Ts...>
+FillTec(const std::decay_t<decltype(get<0>(std::declval<ARIA::tup::detail::Tec<Ts...>>()))> &v) {
+  ARIA::tup::detail::Tec<Ts...> c;
+  ARIA::ForEach<sizeof...(Ts)>([&]<auto i>() {
+    static_assert(std::is_same_v<std::decay_t<decltype(cute::get<i>(c))>, std::decay_t<decltype(v)>>,
+                  "Element types of the `Tec` should be the same");
+    cute::get<i>(c) = v;
+  });
+
+  return c;
+}
+
+} // namespace aria::tup::detail
+
+//
+//
+//
+//! CuTe's original implementation of operators for constant zeros (in `integral_constant.hpp`)
+//! has limitations such as floating point compatibilities.
+//! So, those operators are rewritten by ARIA.
+template <auto t, typename U>
+  requires(t == 0 && std::is_arithmetic_v<U>)
+[[nodiscard]] ARIA_HOST_DEVICE constexpr auto operator*(C<t>, U) {
+  static_assert(t * U(1) == 0, "Invalid types for arithmetic operators");
+  return C<t * U(1)>{};
+}
+
+template <typename U, auto t>
+  requires(t == 0 && std::is_arithmetic_v<U>)
+[[nodiscard]] ARIA_HOST_DEVICE constexpr auto operator*(U, C<t>) {
+  static_assert(U(1) * t == 0, "Invalid types for arithmetic operators");
+  return C<U(1) * t>{};
+}
+
+template <auto t, typename U>
+  requires(t == 0 && std::is_arithmetic_v<U>)
+[[nodiscard]] ARIA_HOST_DEVICE constexpr auto operator/(C<t>, U) {
+  static_assert(t / U(1) == 0, "Invalid types for arithmetic operators");
+  return C<t / U(1)>{};
+}
+
+//
+//
+//
+template <typename... Ts0, typename... Ts1>
+[[nodiscard]] ARIA_HOST_DEVICE constexpr auto operator+(const ARIA::tup::detail::Tec<Ts0...> &lhs,
+                                                        const ARIA::tup::detail::Tec<Ts1...> &rhs) {
+  //! `Ts0` is automatically required to have the same number of elements as `Ts1` at next line.
+  ARIA::tup::detail::Tec<decltype(std::declval<Ts0>() + std::declval<Ts1>())...> res;
+  ARIA::ForEach<sizeof...(Ts0)>([&]<auto i>() { cute::get<i>(res) = cute::get<i>(lhs) + cute::get<i>(rhs); });
+  return res;
+}
+
+template <typename... Ts0, typename... Ts1>
+[[nodiscard]] ARIA_HOST_DEVICE constexpr auto operator-(const ARIA::tup::detail::Tec<Ts0...> &lhs,
+                                                        const ARIA::tup::detail::Tec<Ts1...> &rhs) {
+  ARIA::tup::detail::Tec<decltype(std::declval<Ts0>() - std::declval<Ts1>())...> res;
+  ARIA::ForEach<sizeof...(Ts0)>([&]<auto i>() { cute::get<i>(res) = cute::get<i>(lhs) - cute::get<i>(rhs); });
+  return res;
+}
+
+template <typename... Ts0, typename... Ts1>
+[[nodiscard]] ARIA_HOST_DEVICE constexpr auto operator*(const ARIA::tup::detail::Tec<Ts0...> &lhs,
+                                                        const ARIA::tup::detail::Tec<Ts1...> &rhs) {
+  ARIA::tup::detail::Tec<decltype(std::declval<Ts0>() * std::declval<Ts1>())...> res;
+  ARIA::ForEach<sizeof...(Ts0)>([&]<auto i>() { cute::get<i>(res) = cute::get<i>(lhs) * cute::get<i>(rhs); });
+  return res;
+}
+
+template <typename... Ts0, typename... Ts1>
+[[nodiscard]] ARIA_HOST_DEVICE constexpr auto operator/(const ARIA::tup::detail::Tec<Ts0...> &lhs,
+                                                        const ARIA::tup::detail::Tec<Ts1...> &rhs) {
+  ARIA::tup::detail::Tec<decltype(std::declval<Ts0>() / std::declval<Ts1>())...> res;
+  ARIA::ForEach<sizeof...(Ts0)>([&]<auto i>() { cute::get<i>(res) = cute::get<i>(lhs) / cute::get<i>(rhs); });
+  return res;
+}
+
+template <typename... Ts0, typename Ts1>
+[[nodiscard]] ARIA_HOST_DEVICE constexpr auto operator+(const ARIA::tup::detail::Tec<Ts0...> &lhs, const Ts1 &rhs) {
+  //! `std::conditional_t<...>` at the following line is used to generate such a pack `<Ts1, Ts1, ...>`.
+  return lhs + aria::tup::detail::FillTec<std::conditional_t<true, Ts1, Ts0>...>(rhs);
+}
+
+template <typename... Ts0, typename Ts1>
+[[nodiscard]] ARIA_HOST_DEVICE constexpr auto operator+(const Ts1 &lhs, const ARIA::tup::detail::Tec<Ts0...> &rhs) {
+  return aria::tup::detail::FillTec<std::conditional_t<true, Ts1, Ts0>...>(lhs) + rhs;
+}
+
+template <typename... Ts0, typename Ts1>
+[[nodiscard]] ARIA_HOST_DEVICE constexpr auto operator-(const ARIA::tup::detail::Tec<Ts0...> &lhs, const Ts1 &rhs) {
+  return lhs - aria::tup::detail::FillTec<std::conditional_t<true, Ts1, Ts0>...>(rhs);
+}
+
+template <typename... Ts0, typename Ts1>
+[[nodiscard]] ARIA_HOST_DEVICE constexpr auto operator-(const Ts1 &lhs, const ARIA::tup::detail::Tec<Ts0...> &rhs) {
+  return aria::tup::detail::FillTec<std::conditional_t<true, Ts1, Ts0>...>(lhs) - rhs;
+}
+
+template <typename... Ts0, typename Ts1>
+[[nodiscard]] ARIA_HOST_DEVICE constexpr auto operator*(const ARIA::tup::detail::Tec<Ts0...> &lhs, const Ts1 &rhs) {
+  return lhs * aria::tup::detail::FillTec<std::conditional_t<true, Ts1, Ts0>...>(rhs);
+}
+
+template <typename... Ts0, typename Ts1>
+[[nodiscard]] ARIA_HOST_DEVICE constexpr auto operator*(const Ts1 &lhs, const ARIA::tup::detail::Tec<Ts0...> &rhs) {
+  return aria::tup::detail::FillTec<std::conditional_t<true, Ts1, Ts0>...>(lhs) * rhs;
+}
+
+template <typename... Ts0, typename Ts1>
+[[nodiscard]] ARIA_HOST_DEVICE constexpr auto operator/(const ARIA::tup::detail::Tec<Ts0...> &lhs, const Ts1 &rhs) {
+  return lhs / aria::tup::detail::FillTec<std::conditional_t<true, Ts1, Ts0>...>(rhs);
+}
+
+template <typename... Ts0, typename Ts1>
+[[nodiscard]] ARIA_HOST_DEVICE constexpr auto operator/(const Ts1 &lhs, const ARIA::tup::detail::Tec<Ts0...> &rhs) {
+  return aria::tup::detail::FillTec<std::conditional_t<true, Ts1, Ts0>...>(lhs) / rhs;
+}
+
+} // namespace cute
