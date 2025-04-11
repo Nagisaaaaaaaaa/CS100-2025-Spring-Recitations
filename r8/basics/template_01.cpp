8b8013051db04d51edae534b1aed159b215aa3df
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-04-11 14:42:43 +0800

    .

--- r8/basics/template_01.cpp
+++ r8/basics/template_01.cpp
@@ -16,11 +16,5 @@ struct Vector {
 int main() {
   Vector vec{new int[5]}; // 从今天开始，我们不再用 malloc，而是用 new 和 new[]。
 
-  for (size_t i = 0; i < 5; ++i)
-    vec.data[i] = i;
-
-  for (size_t i = 0; i < 5; ++i)
-    std::cout << vec.data[i] << std::endl;
-
   return 0;
 }
