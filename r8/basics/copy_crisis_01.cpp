228f1d1635b3e04627eba59d70cbfa1fc06eb3eb
Yiheng Wu <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-04-11 20:57:55 +0800

    feat(R8): Add introduction to copy crisis. (#45)

---
+++ r8/basics/copy_crisis_01.cpp
@@ -0,0 +1,30 @@
+/*
+
+  ! Baseline:
+  ! 1. Why copying "RAII owner" is dangerous.
+
+*/
+
+//? 《Ban Dream! It's My构!!!!!》
+
+// 请找到这几行代码中的 bug。
+
+struct Vector {
+  int *data;
+
+  ~Vector() { delete[] data; }
+};
+
+int main() {
+  Vector vec0{new int[5]};
+
+  // 和 C 语言的规则一样，这行代码相当于 vec1.data = vec0.data。
+  Vector vec1 = vec0;
+
+  //! 那么 bug 在哪儿？
+  //!   这两个 Vector 的 data 指向了同一段内存，
+  //!   当它们的析构函数被调用时，会对同一段内存 delete 两次。
+  //!   这会导致 undefined behavior，连 try-catch 都救不了！
+
+  return 0;
+}
---
+++ r8/basics/copy_crisis_02.cpp
@@ -0,0 +1,31 @@
+/*
+
+  ! Baseline:
+  ! 1. `delete` copy constructor and copy assignment operator if
+  !    you don't know how to implement them.
+
+*/
+
+//? 《因为太怕出错就全禁止了。》
+
+struct Vector {
+  int *data;
+
+  //! 禁止拷贝构造：Vector vec1 = vec0。
+  Vector(const Vector &) = delete;
+  //! 禁止拷贝赋值：vec1 = vec0。
+  Vector &operator=(const Vector &) = delete;
+
+  ~Vector() { delete[] data; }
+};
+
+int main() {
+  Vector vec0{new int[5]};
+
+  // Vector vec1 = vec0;
+
+  //! 下集预告：我们将学习如何正确地实现它们，而不是把它们禁止。
+  //!   https://stackoverflow.com/questions/3279543/what-is-the-copy-and-swap-idiom.
+
+  return 0;
+}
