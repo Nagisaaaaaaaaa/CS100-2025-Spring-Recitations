b17b4011056396b079b841c76ac9019c7108519a
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-04-11 14:35:09 +0800

    .

--- r8/basics/template_03.cpp
+++ r8/basics/template_03.cpp
@@ -21,6 +21,32 @@ struct Vector {
   ~Vector() { delete[] data; }
 };
 
+// 你可以想象，编译器或许是这么干的：
+//
+// 当我们写下 Vector<int> 时，编译器把所有的 T 都替换成了 int，生成了一个类型：
+//   struct VectorInt {
+//     int *data;
+//     ~Vector() { delete[] data; }
+//   };
+//
+// 当我们又写下 Vector<double> 时，编译器把所有的 T 都替换成了 double，又生成了一个类型：
+//   struct VectorDouble {
+//     double *data;
+//     ~Vector() { delete[] data; }
+//   };
+//
+// 当我们双写下……
+// 当我们叒写下……
+// 当我们叕写下……
+
+//! 那我问你：
+//!   1. Vector      是一个“类型”吗？
+//!   2. Vector<int> 是一个“类型”吗？
+
+//! 答案是：
+//!   1. Vector      不是一个“类型”，它是一个“模板”。
+//!   2. Vector<int> 是一个“类型”没错。
+
 int main() {
   Vector<int> vecI{new int[5]};
   Vector<double> vecD{new double[5]};
