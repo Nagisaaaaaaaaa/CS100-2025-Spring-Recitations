30103da2bd37345f24ed8fd244592114335657b9
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-04-11 15:24:53 +0800

    .

--- r8/basics/constexpr_01.cpp
+++ r8/basics/constexpr_01.cpp
@@ -2,12 +2,17 @@
 #include <vector>
 
 int main() {
+  //? 《请让我 AC，OJ 大人！》
+
+  //! assert 大法：WA -> RTE。
   assert(0 != 1);
 
+  //! try-catch 大法：RTE -> WA。
   try {
     std::vector<int>(999999999999999999LLU);
   } catch (const std::exception &e) {}
 
+  //! static_assert 大法：凭空创造 compile error。
   static_assert(sizeof(int) == sizeof(unsigned int));
 
   return 0;
