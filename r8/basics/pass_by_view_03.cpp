12793b6f159677f5fbf5c977e7173a602d03aa20
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-04-11 17:23:25 +0800

    .

--- r8/basics/pass_by_view_03.cpp
+++ r8/basics/pass_by_view_03.cpp
@@ -2,8 +2,11 @@
 #include <span>
 #include <vector>
 
-void Print(const std::span<int> &vec) {
-  for (const auto &v : vec)
+//? 《我的函数传参方式果然有问题》
+
+//! view: 大哥们躺着别动就行，让我来当函数参数吧。
+void Print(std::span<int> view) {
+  for (const auto &v : view)
     std::cout << v << " ";
   std::cout << std::endl;
 }
@@ -12,8 +15,12 @@ int main() {
   std::vector<int> vec0 = {0, 1, 2, 3, 4};
   int vec1[5] = {0, 1, 2, 3, 4};
 
+  //! 两个大哥都能够被隐式转化成 non-owning 的 std::span<int> view。
+  //! 最终，view 抗下了所有，打印出来了两个大哥的数组。
   Print(vec0);
   Print(vec1);
+
+  //? (哎呀，我 view 得了 MVP！一看两个大哥什么事都不用干，躺赢狗！)
 
   return 0;
 }
