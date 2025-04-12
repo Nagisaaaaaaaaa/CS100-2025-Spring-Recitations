c118fad8bc23cf2f6a2aad3a08232c594a3efd07
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-04-12 23:49:10 +0800

    .

--- r8/basics/raii_crisis_03.cpp
+++ r8/basics/raii_crisis_03.cpp
@@ -1,10 +1,11 @@
 /*
 
   ! Baseline:
-  ! 1. `delete` copy constructor and copy assignment operator if
-  !    you don't know how to implement them.
+  ! 1. `delete` the 4 methods if you don't know how to implement them.
 
 */
+
+#include <utility>
 
 //? 《因为太怕出错就全禁止了。》
 
@@ -16,6 +17,11 @@ struct Vector {
   //! 禁止拷贝赋值：vec1 = vec0。
   Vector &operator=(const Vector &) = delete;
 
+  //! 禁止“掠夺”构造：Vector vec1 = std::move(vec0)。
+  Vector(Vector &&) = delete;
+  //! 禁止“掠夺”赋值：vec1 = std::move(vec0)。
+  Vector &operator=(Vector &&) = delete;
+
   ~Vector() { delete[] data; }
 };
 
@@ -23,8 +29,10 @@ int main() {
   Vector vec0{new int[5]};
 
   // Vector vec1 = vec0;
+  // Vector vec1 = std::move(vec0);
 
-  //! 下集预告：我们将学习如何正确地实现它们，而不是把它们禁止。
+  //! 下集预告：
+  //!   我们将学习如何正确地实现它们，而不是把它们禁止。
   //!   https://stackoverflow.com/questions/3279543/what-is-the-copy-and-swap-idiom.
 
   return 0;
