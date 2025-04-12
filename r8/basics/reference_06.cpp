582c1931fbb4576d5df38bda92120d42f440f1e0
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-04-12 16:41:27 +0800

    .

--- r8/basics/reference_06.cpp
+++ r8/basics/reference_06.cpp
@@ -12,18 +12,18 @@ private:
 };
 
 int main() {
-  // 问：这行代码拷贝了几次 std::string？
+  //! 那我问你，调用构造函数会拷贝几次 std::string？
   std::string name = "Zhen Ding";
   Student student0(name);
 
-  // 答：
+  // 答案：
   //!  1. 参数是 const std::string &，传入左值，没发生拷贝。
   //   2. name_(name) 做了一次拷贝。
 
-  // 问：这行代码拷贝了几次 std::string？
+  //! 那我问你，调用构造函数会拷贝几次 std::string？
   Student student1(std::string("Zhen Ding"));
 
-  // 答：
+  // 答案：
   //!  1. 参数是 const std::string &，传入“纯净”右值，延长生命周期，没发生拷贝。
   //   2. name_(name) 做了一次拷贝。
 
--- r8/basics/reference_07.cpp
+++ r8/basics/reference_07.cpp
@@ -15,18 +15,18 @@ private:
 };
 
 int main() {
-  // 问：这行代码拷贝了几次 std::string？
+  //! 那我问你，调用构造函数会拷贝几次 std::string？
   std::string name = "Zhen Ding";
   Student student0(name);
 
-  // 答：
+  // 答案：
   //   1. 参数是 std::string name，传入左值，做了一次拷贝。
   //!  2. name_(std::move(name))，直接“掠夺”，没发生拷贝。
 
-  // 问：这行代码拷贝了几次 std::string？
+  //! 那我问你，调用构造函数会拷贝几次 std::string？
   Student student1(std::string("Zhen Ding"));
 
-  // 答：
+  // 答案：
   //!  1. 参数是 std::string name，传入“纯净”右值，没发生拷贝。
   //!  2. name_(std::move(name))，直接“掠夺”，没发生拷贝。
 
