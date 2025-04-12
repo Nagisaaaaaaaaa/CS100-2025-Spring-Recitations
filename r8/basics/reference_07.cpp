9789d0a47c55bbf5e5ecac4f9d428621c4a914dd
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-04-12 22:09:40 +0800

    .

--- r8/basics/reference_07.cpp
+++ r8/basics/reference_07.cpp
@@ -16,8 +16,9 @@ public:
   //!   2. 调用 std::move。
   //!
   //! name_(std::move(name)) 的意思是：
-  //!   让 name_ 直接“夺舍”掉 name，从此，name 名存实亡！
-  //!   所以，std::move 这个函数的意思是“夺舍”，跟“移动”没有任何关系。
+  //!   让 name_ 直接“掠夺” name，从此，name 名存实亡！
+  //!   所以，std::move 这个函数的意思是“把拷贝换成掠夺”，
+  //!   跟“移动”没有任何关系。
   explicit Student(std::string name) : name_(std::move(name)) {}
 
 private:
@@ -31,14 +32,14 @@ int main() {
 
   // 答案：
   //   1. 参数是 std::string，传入左值，一次拷贝。
-  //!  2. name_(std::move(name))，直接“夺舍”，没有拷贝。
+  //!  2. name_(std::move(name))，直接“掠夺”，没有拷贝。
 
   // 那我问你，调用构造函数会拷贝几次 std::string？
   Student student1(std::string("Zhen Ding"));
 
   // 答案：
   //!  1. 参数是 std::string，“孤魂野鬼找地方住”，没有拷贝。
-  //!  2. name_(std::move(name))，直接“夺舍”，没有拷贝。
+  //!  2. name_(std::move(name))，直接“掠夺”，没有拷贝。
 
   return 0;
 }
