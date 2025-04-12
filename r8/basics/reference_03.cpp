3aa35d7f9eb6a67c3ea6c1d4ba74714c8bbb3ae9
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-04-12 15:47:16 +0800

    .

--- r8/basics/reference_03.cpp
+++ r8/basics/reference_03.cpp
@@ -1,30 +1,14 @@
-//? 《右值终末旅行》
-
 #include <string>
 
-class Student {
-public:
-  explicit Student(std::string name) : name_(name) {}
-
-private:
-  std::string name_;
-};
+//? 《右值终末旅行》
 
 int main() {
-  // 问：这行代码拷贝了几次 std::string？
-  std::string name = "Zhen Ding";
-  Student student0(name);
+  // 等号的右手边，构造了一个 std::string。
+  //! 那我问你，这里的等号，调用了什么函数？Copy？
+  std::string str = std::string("QAQ");
 
-  //! 答：
-  //!   1. 构造函数的参数写的是 std::string name，所以传入时做了一次拷贝。
-  //!   2. name_(name) 又做了一次拷贝。
-
-  // 问：这行代码拷贝了几次 std::string？
-  Student student1("Zhen Ding");
-
-  //! 答：
-  //!   1. 构造函数的参数写的是 std::string name，所以传入时做了一次拷贝。
-  //!   2. name_(name) 又做了一次拷贝。
+  //! 原因很复杂，我们今天只说结论：
+  //!   这 个 等 号 没 有 调 用 任 何 函 数！
 
   return 0;
 }
