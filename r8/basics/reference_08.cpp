750892b7b9ca792826365d9d5a3a363b9eff0b0d
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-04-12 22:21:12 +0800

    .

--- r8/basics/reference_08.cpp
+++ r8/basics/reference_08.cpp
@@ -6,26 +6,25 @@
 */
 
 #include <iostream>
-#include <vector>
 
 int main() {
   {
-    // 默认情况下，v1 = v0 会做个拷贝，并且是个深拷贝。
-    std::vector<int> v0(10);
-    std::vector<int> v1 = v0;
+    // 默认情况下，str1 = str0 会做个拷贝，并且是个深拷贝。
+    std::string str0("QAQ");
+    std::string str1 = str0;
 
-    std::cout << v0.size() << std::endl;
-    std::cout << v1.size() << std::endl;
+    std::cout << str0.length() << std::endl;
+    std::cout << str1.length() << std::endl;
   }
 
   {
     //! 但是，使用 std::move，我们就能“把拷贝换成掠夺”。
-    std::vector<int> v0(10);
-    std::vector<int> v1 = std::move(v0);
+    std::string str0("QAQ");
+    std::string str1 = std::move(str0);
 
-    //! 这里 v0 已经被“掠夺”了，一无所有，所以 size 是 0。
-    std::cout << v0.size() << std::endl;
-    std::cout << v1.size() << std::endl;
+    //! 这里 str0 已经被“掠夺”了，一无所有，所以 length 是 0。
+    std::cout << str0.length() << std::endl;
+    std::cout << str1.length() << std::endl;
   }
 
   return 0;
