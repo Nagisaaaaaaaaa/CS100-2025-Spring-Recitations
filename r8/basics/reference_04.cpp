576b87f35e98ff9fb68d5479bfe04869cf34e950
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-04-12 15:55:24 +0800

    .

--- r8/basics/reference_04.cpp
+++ r8/basics/reference_04.cpp
@@ -2,12 +2,17 @@
 
 //? 《右值终末旅行》
 
-void Func(std::string) {}
+void Func0(std::string) {}
+
+void Func1(const std::string &) {}
 
 int main() {
-  // 我们先构造了一个 std::string，然后将它传给 Func。
+  // 我们先构造了一个 std::string，然后将它传给 Func0。
   //! 那我问你，传参的时候，有没有拷贝？调用了什么函数？
-  Func(std::string("QAQ"));
+  Func0(std::string("QAQ"));
+
+  // 那如果用 const & 延长生命周期呢？
+  Func1(std::string("QAQ"));
 
   //! 原因很复杂，我们今天只说结论：
   //!   传 参 的 时 候 没 有 调 用 任 何 函 数！
