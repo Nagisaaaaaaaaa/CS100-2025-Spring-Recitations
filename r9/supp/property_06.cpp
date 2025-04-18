be645f18469db9f808b8d4e0cbc7f819cdb49a24
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-04-19 02:38:26 +0800

    .

--- r9/supp/property_06.cpp
+++ r9/supp/property_06.cpp
@@ -29,12 +29,12 @@ int main() {
   std::cout << typeid(vec1[0]).name() << std::endl;
 
   //! std::vector<bool> 几乎是 STL 中最“诡异”的类型。
-  //! 我们知道，C/C++ 中，bool 这个类型至少也得有 1 个 byte。
-  //! 但 std::vector<bool> 实际上使用 1 个 bit 来存储 bool，
-  //! 这么做是为了节省内存。
+  //! 我们知道，C++ 中，bool 这个类型至少也得有 1 个 byte。
+  //! 但 std::vector<bool> 实际上只使用 1 个 bit 来存储 bool，
+  //! 这么设计是为了节省内存。
   //!
-  //! 所以，std::vector<bool> 的中括号运算符，
-  //! 返回的是那个 bool 的“代理”。
+  //! 可以想象，std::vector<bool> 的中括号运算符，
+  //! 返回的是某个 bool 的“代理”。
   //! 和 student.year() 非常类似。
 
   return 0;
