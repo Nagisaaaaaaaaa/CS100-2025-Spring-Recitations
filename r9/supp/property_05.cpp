e54255531220665e1fd7f972df8e4e4a992c2517
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-04-18 18:47:34 +0800

    .

--- r9/supp/property_05.cpp
+++ r9/supp/property_05.cpp
@@ -2,6 +2,9 @@
 
 using namespace ARIA;
 
+//? 《Re：从零开始的大一生活》
+
+//* 我们来直观地感受一下“代理” (proxy) 的强大之处。
 class Student {
 public:
   const std::string &name() const { return name_; }
@@ -12,17 +15,37 @@ public:
 
   int &id() { return id_; }
 
+  //* 不知名的野鸡开源库 ARIA 里面有 Property.h 这个头文件，
+  //* 它提供了一个名叫 ARIA_PROP 的宏，
+  //* 可以用来创建各种“代理”。
+  //*
+  //* 比如这里我们创建了一个名叫 year，类型为 int 的代理。
+  //* (别在意这里的前 3 个参数，这么写就对了)
   ARIA_PROP(public, public, ARIA_HOST, int, year);
 
 private:
+  //* 注意，在这个例子中，我们只存储 id，不存储 year。
   std::string name_;
   int id_ = 0;
 
+  //* 我们希望能这么写代码：
+  //*   int y = student.year();
+  //*
+  //* 这意味着，每次都需要当场根据 id 计算 year。
+  //* 只需要为 year 这个“代理”定义一个叫做“getter”的神秘函数，
+  //* year 就能够自动支持这个功能了。
   int ARIA_PROP_GETTER(year)() const {
     // 取学号的前 4 位。
     return id_ / 1000000;
   }
 
+  //* 我们还希望能这么写代码：
+  //*   student.year() = y;
+  //*   student.year() -= 4;
+  //*
+  //* 因为我们只存储了 id，所以需要根据给定的 year 修改 id。
+  //* 只需要为 year 这个“代理”定义一个叫做“setter”的神秘函数，
+  //* year 就能够自动支持所有相关功能，包括 += 和 -=。
   void ARIA_PROP_SETTER(year)(const int &value) {
     // 修改学号的前 4 位。
     id_ = id_ % 1000000 + value * 1000000;
