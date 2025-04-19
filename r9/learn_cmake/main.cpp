200e27e5c1e47edc180009465997a4e2bda48b2b
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-04-19 23:35:11 +0800

    .

--- r9/learn_cmake/main.cpp
+++ r9/learn_cmake/main.cpp
@@ -4,8 +4,8 @@
 using namespace glm;
 
 int main() {
-  vec3 a(1, 0, 0);
-  vec3 b(0, 1, 0);
+  vec3 a{1, 0, 0};
+  vec3 b{0, 1, 0};
   vec3 c = cross(a, b);
 
   printf("c = (%f, %f, %f)\n", c.x, c.y, c.z);
