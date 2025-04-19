235783abf1d5e9111cd09dfad828de0285369e3f
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-04-19 22:48:49 +0800

    .

--- r9/learn_cmake/run.py
+++ r9/learn_cmake/run.py
@@ -1,4 +1,4 @@
 import subprocess
 
 subprocess.run(["cmake", "--build", "build", "-j"], check=True)
-subprocess.run(["./LearnCMake.exe"], check=True)
+subprocess.run(["./bin/LearnCMake.exe"], check=True)
