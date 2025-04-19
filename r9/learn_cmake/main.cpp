12649c766d2a01e5498149fbe90c87bb3a93a285
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-04-19 23:05:52 +0800

    .

--- r9/learn_cmake/main.cpp
+++ r9/learn_cmake/main.cpp
@@ -1,5 +1,12 @@
+#include <glm/glm.hpp>
 #include <iostream>
 
+using namespace glm;
+
 int main() {
-  std::cout << "Hello CMake!" << std::endl;
+  vec3 a(1, 0, 0);
+  vec3 b(0, 1, 0);
+  vec3 c = cross(a, b);
+
+  printf("c = (%f, %f, %f)\n", c.x, c.y, c.z);
 }
