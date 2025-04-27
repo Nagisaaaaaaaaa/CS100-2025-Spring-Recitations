ea0bab19a33449d4772bc887b5e51ff6e096f40a
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-04-27 18:53:13 +0800

    .

---
+++ r10/basics/smart_pointers_01.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <memory>
+
+class Window {
+public:
+  Window(int width, int height)
+      : width_(width)
+      , height_(height) {}
+
+  ~Window() { std::cout << "Destructing the window" << std::endl; }
+
+public:
+  int width() const { return width_; }
+
+  int height() const { return height_; }
+
+private:
+  int width_;
+  int height_;
+};
+
+int main() {
+  {
+    std::unique_ptr<Window> window = std::make_unique<Window>(2560, 1440);
+    std::cout << "Width : " << window->width() << std::endl;
+    std::cout << "Height: " << window->height() << std::endl;
+  }
+
+  {
+    auto window = std::make_shared<Window>(2560, 1440);
+    // ...
+  }
+
+  return 0;
+}
