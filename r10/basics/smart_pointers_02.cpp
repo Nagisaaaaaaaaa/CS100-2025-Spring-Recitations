9d6bb9319de6a77202a77e28c911d9fc1f1d7171
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-04-27 19:34:14 +0800

    .

--- r10/basics/smart_pointers_02.cpp
+++ r10/basics/smart_pointers_02.cpp
@@ -44,5 +44,17 @@ int main() {
     Window *ptr = window.get();
   }
 
+  // 有没有感受到一点违和？
+  //   auto window0 = std::make_shared<Window>(2560, 1440);
+  //   auto window1 = window0;
+  //
+  //! 谁真正拥有这个窗口？
+  //   window0 和 window1 都拥有这个窗口。
+  //   我们目前为止看到所有的 RAII 例子中，都只有一个“所有者” (owner)。
+  //   这是第一次出现了多个“所有者”。
+  //
+  //! 谁负责调用 Window 的析构函数？
+  //   最后一个拥有 Window 的“所有者”会调用析构析构函数。
+
   return 0;
 }
