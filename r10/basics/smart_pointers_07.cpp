78d06189a5e1e260aafd6cb88cfaeae6305b90a8
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-04-28 22:37:02 +0800

    .

--- r10/basics/smart_pointers_07.cpp
+++ r10/basics/smart_pointers_07.cpp
@@ -1,4 +1,18 @@
+/*
+
+  ! 请使用 g++ -std=c++20 来编译。
+
+  ! 我们已经学到了哪几种“资源管理”方法？
+  ! Baseline:
+  ! 1. 存在唯一“所有者”的管理方法 (纯爱战士)。
+  ! 2. 存在多个“所有者”的管理方法 (牛头人)。
+  * Bonus:
+  * 3. 不存在任何“所有者”的管理方法 (散养)？见 r7/supp。
+
+*/
+
 #include "../../r7/supp/ARIA/Registry.h"
+
 #include <memory>
 #include <vector>
 
@@ -32,14 +46,14 @@ int main() {
   // 至此，我们已经学到了这几种“内存管理”方法，
   // 或者更通用的，我们称之为“资源管理”方法。
 
-  //! 1. 拥有唯一“所有者”的管理方法 (纯爱战士)。
+  //! 1. 存在唯一“所有者”的管理方法 (纯爱战士)。
   std::vector<Window> windows0;
   std::unique_ptr<Window> window1;
 
-  //! 2. 拥有多个“所有者”的管理方法 (牛头人)。
+  //! 2. 存在多个“所有者”的管理方法 (牛头人)。
   std::shared_ptr<Window> window2;
 
-  //* 3. 不存在“所有者”的管理方法 (野生)？见 r7/supp。
+  //* 3. 不存在任何“所有者”的管理方法 (散养)？见 r7/supp。
   Window *window3 = new Window{2560, 1440};
   Window *window4 = new Window{3840, 2160};
 
@@ -52,14 +66,18 @@ int main() {
     std::cout << "Height: " << window.height() << std::endl;
   }
 
-  //* 我们说过，在 C++ 中不建议手动 new 和 delete，
-  //* 但为什么 Registry 能够让这件事情变得很安全？
+  //* 有趣的是，Registry 能够让 new 和 delete 变得非常安全。
   while (Window::size() > 0) {
     Window &window = *Window::begin();
     delete &window;
   }
 
   std::cout << Window::size() << std::endl;
+
+  //! 那么如何选择该使用哪种管理方式呢？
+  //! 1. 请永远优先选择“纯爱战士”。
+  //* 2. 如果觉得“散养”很香，也可以选择它。
+  //! 3. 请尽量不要选择“牛头人”。
 
   return 0;
 }
