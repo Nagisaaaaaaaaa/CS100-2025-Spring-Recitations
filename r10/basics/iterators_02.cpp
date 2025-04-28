c29aa37e339d9c36e6a74fcd61ca946ec0e22ea0
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-04-28 16:19:08 +0800

    .

--- r10/basics/iterators_02.cpp
+++ r10/basics/iterators_02.cpp
@@ -9,6 +9,7 @@ int main() {
   std::list<int> arr1{0, 1, 2, 3, 4};
 
   // 我们来显式地看一下 begin 和 end 的返回值类型。
+  // 它们的类型名字有点长，但都是以“iterator”作为结尾的。
 
   std::vector<int>::iterator begin0 = arr0.begin();
   std::vector<int>::iterator end0 = arr0.end();
@@ -16,8 +17,13 @@ int main() {
   std::list<int>::iterator begin1 = arr1.begin();
   std::list<int>::iterator end1 = arr1.end();
 
-  // “iterator”的中文是“迭代器”，也就是说，
-  //! STL 要求我们总是使用迭代器来遍历所有的“容器” (数组、链表)。
+  // “iterator”的中文是“迭代器”，
+  //! 我们总是使用迭代器来遍历“容器” (数组、链表)。
+  for (auto it = begin0; it != end0; ++it)
+    std::cout << *it << std::endl;
+
+  for (auto it = begin1; it != end1; ++it)
+    std::cout << *it << std::endl;
 
   // 那你会问，我不也可以用传统方法遍历数组嘛：
   for (size_t i = 0; i < arr0.size(); ++i)
@@ -28,7 +34,7 @@ int main() {
     std::cout << arr1[i] << std::endl;
 
   // 这是因为链表只能“按照顺序一个一个访问”，不能“跳着访问”。
-  //! 这就是为什么，STL 要求我们总是使用迭代器来遍历所有的“容器” (数组、链表)。
+  //! STL 的设计是尽可能追求“大一统”的。
 
   return 0;
 }
