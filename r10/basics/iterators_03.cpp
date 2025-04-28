da26b6581a3b0bde56ce105e4aa1f7228fa0bf29
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-04-28 16:31:20 +0800

    .

--- r10/basics/iterators_03.cpp
+++ r10/basics/iterators_03.cpp
@@ -26,12 +26,17 @@ int main() {
   for (size_t i = 0; i < vec.size; ++i)
     vec.data[i] = i;
 
-  //! 我们竟然就可以像 std::vector 和 std::list 一样使用它了！
+  // 我们竟然就可以像 std::vector 和 std::list 一样使用它了！
   for (auto it = vec.begin(); it != vec.end(); ++it)
     std::cout << *it << std::endl;
 
+  //! 任何 class，只要实现了 begin 和 end 这两个成员函数，
+  //! 就能够自动支持 range-based for 循环。
   for (const auto &v : vec)
     std::cout << v << std::endl;
+
+  //! 所以，对于数组来说，裸指针本身就可以被作为一种“迭代器”。
+  // 但对于其他更复杂的数据结构，裸指针就不够用了。
 
   return 0;
 }
