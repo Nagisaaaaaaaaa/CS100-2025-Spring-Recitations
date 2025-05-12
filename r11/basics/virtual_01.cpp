7342c7ff2f5862b26e77f62220b8ba6c5d6e9575
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-05-12 16:15:15 +0800

    .

--- r11/basics/virtual_01.cpp
+++ r11/basics/virtual_01.cpp
@@ -3,13 +3,25 @@
 
 //? 《OLD GAME!》
 
+// 假设我们想使用 C++ 来实现 HW4 坦克大战，
+// 我们已经逐渐了解了什么是面向对象编程。
+
+// GameObject 是场景中所有“游戏对象”类型的父类。
+// 场景中所有的 GameObject 在每一帧都会
+// 调用一次函数 Update，来更新自己的状态。
+
+// Tank   的 Update 负责处理 移动、碰撞、射击……
+// Bullet 的 Update 负责处理 移动、碰撞、破坏……
+
 class GameObject {
 public:
+  // 我们为 Update 定义了默认行为。
   void Update() { std::cout << "GameObject::Update" << std::endl; }
 };
 
 class Tank : public GameObject {
 public:
+  // 我们为 Tank 量身定制了 Update，覆盖掉默认行为。
   void Update() { std::cout << "Tank::Update" << std::endl; }
 };
 
