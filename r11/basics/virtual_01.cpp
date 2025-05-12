4a55ba30bfd501a99ab98e2df4af64485926b9ff
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-05-12 13:54:04 +0800

    .

---
+++ r11/basics/virtual_01.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+class GameObject {
+public:
+  void Update() { std::cout << "GameObject::Update" << std::endl; }
+};
+
+class Tank : public GameObject {
+public:
+  void Update() { std::cout << "Tank::Update" << std::endl; }
+};
+
+int main() {
+  Tank tank;
+
+  Tank *tankPtr = &tank;
+  GameObject *gameObjectPtr = &tank;
+
+  // 那我问你，这两行代码会打印出什么？
+  tankPtr->Update();
+  gameObjectPtr->Update();
+
+  // 这不是我们想要的，因为假设我们在设计一个游戏引擎，
+  // 肯定希望这么写代码：
+  std::vector<GameObject *> gameObjects;
+  for (auto *gameObject : gameObjects)
+    gameObject->Update(); // 永远只会调用父类的 Update 啊。
+
+  // 更具体地说，我们希望能够：
+  //! 使用父类的指针，调用子类的函数。
+
+  return 0;
+}
