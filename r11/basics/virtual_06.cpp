bc5d0887a0607c0a9d815e0e3cac36fa2b538404
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-05-12 17:02:30 +0800

    .

--- r11/basics/virtual_06.cpp
+++ r11/basics/virtual_06.cpp
@@ -20,12 +20,17 @@ public:
 };
 
 int main() {
+  // 至此，我们已经学会如何
+  //! 使用父类的指针，调用子类的函数。
+  // 既然这个环节必须用到指针，就不得不提智能指针。
+
   // 实际上很多时候，虚函数会配合智能指针一起使用。
   std::vector<std::unique_ptr<GameObject>> gameObjects;
 
   gameObjects.push_back(std::make_unique<Tank>());
   gameObjects.push_back(std::make_unique<Bullet>());
 
+  //! 那我问你，这两行代码会打印出什么？
   for (auto &gameObject : gameObjects)
     gameObject->Update();
 
