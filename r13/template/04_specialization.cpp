5208016c680f91fd6f52cad3b03134a19d6ce4a8
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-05-23 19:57:25 +0800

    .

---
+++ r13/template/04_specialization.cpp
@@ -0,0 +1,43 @@
+#include <cstdint>
+
+//? 《Template/stay night》
+
+//* 还记得远在 r09/supp，我们介绍了“代理” (proxy)。
+//* 当时提到，std::vector<bool> 实际上只使用 1 个 bit 来存储 bool，
+//* 这么设计是为了节省内存。
+
+//* 那么，如何为我们的 Dynarray 实现类似的功能呢？
+//! 也就是说，我们希望特殊处理 模板参数为 bool 的情况。
+//! 而 C++ 允许“模板特化” (template specialization)，
+//! 因此能够很轻松地做到这件事情。
+
+// 这是我们的默认情况。
+template <typename T>
+class Dynarray {
+public:
+  // ...
+
+private:
+  T *data_ = nullptr;
+  size_t size_ = 0;
+};
+
+//! 模板特化：特殊处理 模板参数为 bool 的情况
+template <>
+class Dynarray<bool> {
+public:
+  //! 允许定义不同的成员函数。
+  // ...
+
+private:
+  //! 允许定义不同的成员变量。
+  uint32_t *data_ = nullptr;
+  size_t size_ = 0;
+};
+
+int main() {
+  // 模板参数为 int 时，会优先选择“默认”的版本。
+  Dynarray<int> v0;
+  // 模板参数为 bool 时，会优先选择“特化”的版本。
+  Dynarray<bool> v1;
+}
